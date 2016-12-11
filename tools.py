94854eae90ae431986c539901c200644b5965099
Alpha Renner <alpren@ini.uzh.ch>
2016-12-11 16:16:38 +0100

    add a first example how neuron equations could be constructed, add tools.py

---
+++ tools.py
@@ -0,0 +1,15 @@
+from brian2 import *
+import numpy as np
+
+# function that calculates 1D index from 2D index
+@implementation('numpy', discard_units=True)
+@check_units(x0=1,x1=1,n2dNeurons=1,result=1)
+def xy2ind(x0,x1,n2dNeurons):
+    return int(x0)+int(x1)*n2dNeurons
+
+# function that calculates 2D index from 1D index
+@implementation('numpy', discard_units=True)
+@check_units(ind=1,n2dNeurons=1,result=1)
+def ind2xy(ind,n2dNeurons):
+    ret = (np.mod(np.round(ind),n2dNeurons), np.floor_divide(np.round(ind),n2dNeurons))     
+    return ret

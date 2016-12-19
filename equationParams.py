344353eda8cfbf4b3c3cfc913c502461efc8a762
Alpha Renner <alpren@ini.uzh.ch>
2016-12-19 13:44:59 +0100

    add Gerstner neuron equation with all constants and add parameter file and function to load parameter file into neurongroup

---
+++ equationParams.py
@@ -0,0 +1,52 @@
+'''
+This file contains default parameters for different neuron equations as a dict.
+use tools.setparams(group,paramdict) to set them to a neurongroup
+
+Created on 17.12.2016
+
+@author: Alpha
+'''
+
+from brian2 import *
+
+
+gerstnerExpAIFdefaultregular = {"tauIe"  : 5     *ms,
+                                "tauIi"  : 7     *ms,
+                                "C"      : 281   *pF,
+                                "gL"     : 35    *nS,
+                                "EL"     : -70.6 *mV,
+                                "VT"     : -50.4 *mV,
+                                "DeltaT" : 2     *mV,
+                                "tauw"   : 144   *ms,
+                                "a"      : 4     *nS,
+                                "b"      : 0.0805*nA,
+                                "Vr"     : -70.6 *mV,
+                                "Vm"     : -70.6 *mV}
+               
+gerstnerExpAIFdefaultbursting ={"tauIe"  : 5     *ms,
+                                "tauIi"  : 7     *ms,
+                                "C"      : 281   *pF,
+                                "gL"     : 35    *nS,
+                                "EL"     : -70.6 *mV,
+                                "VT"     : -50.4 *mV,
+                                "DeltaT" : 2     *mV,
+                                "tauw"   : 20    *ms,
+                                "a"      : 4     *nS,
+                                "b"      : 0.5   *nA,
+                                "Vr"     : -45.4 *mV,
+                                "Vm"     : -70.6 *mV}
+
+gerstnerExpAIFdefaultfast   =  {"tauIe"  : 5     *ms,
+                                "tauIi"  : 7     *ms,
+                                "C"      : 281   *pF,
+                                "gL"     : 35    *nS,
+                                "EL"     : -70.6 *mV,
+                                "VT"     : -50.4 *mV,
+                                "DeltaT" : 2     *mV,
+                                "tauw"   : 144   *ms,
+                                "a"      : 3.9   *nS,
+                                "b"      : 0     *nA,
+                                "Vr"     : -70.6 *mV,
+                                "Vm"     : -70.6 *mV} 
+
+

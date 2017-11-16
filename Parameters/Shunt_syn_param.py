3bc36716915c729054e6905fd3fc9491d0eb69c2
marcorax <marcorax@ini.uzh.ch>
2017-11-16 14:24:34 +0100

    Final model for DPI synapses provided, some parameters names has changed, added shunt synapses but testing needed

--- Parameters/Shunt_syn_param.py
+++ Parameters/Shunt_syn_param.py
@@ -3,18 +3,14 @@ from brian2 import ms, mV, pA, nS, nA, pF, us, volt, second
 parameters = {
     'Csyn': 0.100 * pF,
     'Io_syn': 0.5 * pA,
-    'Ie_tau': 1. * pA,
     'Ii_tau': 1. * pA,
     'Ut_syn': 25. * mV,
     'Vdd_syn': 1.8 * volt,
     'Vth_syn': 1.7 * volt,
-    'baseweight_e': 7. * pA,
     'baseweight_i': 7. * pA,
     'kn_syn': 0.75,
     'kp_syn': 0.66,
     'wPlast': 1,
-    'Ie_th' : 1 * pA,
     'Ii_th' : 1 * pA,
-    'Ie_syn' : 0.5 * pA,
     'Ii_syn' : 0.5 * pA 
 }

e12a0553a8ea3bc9d3d42ff97e451899c3e59a00
marcorax <marcorax@ini.uzh.ch>
2017-11-30 14:31:22 +0100

    Set better parameters for dpi synapse

--- Equations/SynapseEquation.py
+++ Equations/SynapseEquation.py
@@ -342,17 +342,16 @@ Dpi = {'model': '''
 
 # standard parameters for Dpi models
 DpiPara = {
-    'Csyn': 0.5 * pF,
+    'Csyn': 1.5 * pF,
     'Io_syn': 0.5 * pA,
-    'Ie_tau': 7. * pA,
-    'Ii_tau': 7. * pA,
+    'Ie_tau': 10. * pA,
+    'Ii_tau': 10. * pA,
     'Ut_syn': 25. * mV,
-    'baseweight_e': 7. * pA,
-    'baseweight_i': 7. * pA,
+    'baseweight_e': 50. * pA,
+    'baseweight_i': 50. * pA,
     'kn_syn': 0.75,
     'kp_syn': 0.66,
     'wPlast': 1,
-    "Igain": 15 * pA,
     'Ie_th': 10 * pA,
     'Ii_th': 10 * pA,
     'Ie_syn': 0.5 * pA,
--- Parameters/dpi_synapse_param.py
+++ Parameters/dpi_synapse_param.py
@@ -1,17 +1,16 @@
 from brian2 import ms, mV, pA, nS, nA, pF, us, volt, second
 
 parameters = {
-    'Csyn': 0.5 * pF,
+    'Csyn': 1.5 * pF,
     'Io_syn': 0.5 * pA,
-    'Ie_tau': 7. * pA,
-    'Ii_tau': 7. * pA,
+    'Ie_tau': 10. * pA,
+    'Ii_tau': 10. * pA,
     'Ut_syn': 25. * mV,
-    'baseweight_e': 7. * pA,
-    'baseweight_i': 7. * pA,
+    'baseweight_e': 50. * pA,
+    'baseweight_i': 50. * pA,
     'kn_syn': 0.75,
     'kp_syn': 0.66,
     'wPlast': 1,
-    "Igain": 15 * pA,
     'Ie_th': 10 * pA,
     'Ii_th': 10 * pA,
     'Ie_syn': 0.5 * pA,

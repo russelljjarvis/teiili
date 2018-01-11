cc2fe5cd2de8a074615031874412e83984d55ed6
mmilde <mmilde@ini.uzh.ch>
2018-01-11 12:45:39 +0100

    Wrote first few unittest for WTA bb.

---
+++ unit_tests/test_wta.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Author: mmilde
+# @Date:   2018-01-08 16:30:52
+# @Last Modified by:   mmilde
+# @Last Modified time: 2018-01-11 12:43:19
+import unittest
+import numpy as np
+from NCSBrian2Lib.building_blocks.wta import WTA
+from brian2 import prefs, ms
+
+prefs.codegen.target = "numpy"
+#TODO: test plotting;
+
+
+class TestWTA(unittest.TestCase):
+
+    def test_attributes_1D(self):
+        test1DWTA = WTA(name='test1DWTA', dimensions=1, num_neurons=16, debug=False)
+        self.assertEqual(test1DWTA.inputGroup.N, 16)
+        self.assertEqual(test1DWTA.num_neurons, 16)
+        self.assertEqual(test1DWTA.Groups['gWTAGroup'].N, 16)
+
+    def test_attributes_2D(self):
+        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, debug=False)
+        self.assertEqual(test2DWTA.inputGroup.N, 16**2)
+        self.assertEqual(test2DWTA.num_neurons, 16)
+        self.assertEqual(test2DWTA.Groups['gWTAGroup'].N, 16**2)
+
+    def test_set_spikes(self):
+        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, debug=False)
+        neuron_ts = np.arange(0, 51, 5)
+        neuron_id = np.ones((len(neuron_ts))) * (16**2 / 2)
+        test2DWTA.inputGroup.set_spikes(indices=neuron_id, times=neuron_ts * ms)
+        self.assertEqual((np.asarray(test2DWTA.inputGroup._neuron_index)).tolist(), neuron_id.tolist())
+        self.assertEqual((np.asarray(test2DWTA.inputGroup._spike_time / ms)).tolist(), neuron_ts.tolist())
+
+
+if __name__ == '__main__':
+    unittest.main()

292e18ea5c3504cb11742dddb5c13204e5351980
mmilde <mmilde@ini.uzh.ch>
2018-01-17 15:46:16 +0100

    Added Headers and docstrings to pre-defined neuron and synapse equation files

--- models/equations/dpi_neuron.py
+++ models/equations/dpi_neuron.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: mrax, mmilde
+# @Date:   2017-12-27 10:46:44
+# @Last Modified by:   mmilde
+# @Last Modified time: 2018-01-17 15:40:37
+
+"""File contains a pre-defined DPI neuron model with all its attributes required by brian2.
+This model was orinally pubished in Chicca et al. 2014
+
+Attributes:
+    dpi_neuron_eq (dict): Dictionary which specifies neuron according to Differential Pair Intgrator (DPI)
+        neuron circuit.
+"""
 dpi_neuron_eq = {'model': '''
                         dImem/dt = (((Ith_clip / Itau_clip) * (Iin_clip  + Ia_clip - Ishunt - Iahp_clip)) - Ith_clip - ((1 + ((Ishunt + Iahp_clip - Ia_clip) / Itau_clip)) * Imem)   ) / (tau * ((Ith_clip/(Imem + Io)) + 1)) : amp (unless refractory)
 

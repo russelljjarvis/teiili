f1819ff7c7c882014438f53c9f7e9268fc05f9dc
Moritz Milde <mmilde@ini.uzh.ch>
2018-03-16 19:34:59 +0100

    Formatted the code according to pep8

--- unit_tests/example_dvs_plot.py
+++ unit_tests/example_dvs_plot.py
@@ -23,23 +23,23 @@ eventsfile_Cam1 = filedialog.askopenfilename()
 eventsfile_Cam2 = filedialog.askopenfilename()
 spmon2d_Cam1 = Plotter2d.loaddvs(eventsfile_Cam1)
 spmon2d_Cam2 = Plotter2d.loaddvs(eventsfile_Cam2)
-imv1 = spmon2d_Cam1.plot3d_on_off(plot_dt=10*ms,filtersize = 100*ms)
-imv2 = spmon2d_Cam2.plot3d_on_off(plot_dt=10*ms,filtersize = 50*ms)
+imv1 = spmon2d_Cam1.plot3d_on_off(plot_dt=10 * ms, filtersize=100 * ms)
+imv2 = spmon2d_Cam2.plot3d_on_off(plot_dt=10 * ms, filtersize=50 * ms)
 
-imv3 = spmon2d_Cam1.plot3d(plot_dt=10*ms,filtersize = 200*ms)
-imv4 = spmon2d_Cam2.plot3d(plot_dt=10*ms,filtersize = 200*ms)
-#imv1.show()
+imv3 = spmon2d_Cam1.plot3d(plot_dt=10 * ms, filtersize=200 * ms)
+imv4 = spmon2d_Cam2.plot3d(plot_dt=10 * ms, filtersize=200 * ms)
+# imv1.show()
 
-## Create window with ImageView widget
+# Create window with ImageView widget
 win = QtGui.QDialog()
 #gridlayout =  QtGui.QHBoxLayout(win)
 #gridlayout =  QtGui.QVBoxLayout(win)
-gridlayout =  QtGui.QGridLayout(win)
-gridlayout.addWidget(imv1,1,1)
-gridlayout.addWidget(imv2,2,1)
-gridlayout.addWidget(imv3,1,2)
-gridlayout.addWidget(imv4,2,2)
-win.resize(1500,1000)
+gridlayout = QtGui.QGridLayout(win)
+gridlayout.addWidget(imv1, 1, 1)
+gridlayout.addWidget(imv2, 2, 1)
+gridlayout.addWidget(imv3, 1, 2)
+gridlayout.addWidget(imv4, 2, 2)
+win.resize(1500, 1000)
 win.setLayout(gridlayout)
 win.show()
 win.setWindowTitle('DVS plot')
@@ -50,5 +50,5 @@ imv4.play(50)
 
 #spmon2d_Cam1.plot_panes(num_panes=40, filtersize=100 * ms, num_rows=4, filename=None)
 
-#please provide an absolute path, otherwise dvs.gif will be stored in your wd!
+# please provide an absolute path, otherwise dvs.gif will be stored in your wd!
 #spmon2d_Cam1.generate_gif('dvs.gif', filtersize=100 * ms, plot_dt=50 * ms)

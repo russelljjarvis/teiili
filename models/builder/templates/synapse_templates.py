3f0143b814619c3efb857f91cf2aa5f7a1de23a2
mmilde <mmilde@ini.uzh.ch>
2018-05-28 19:26:57 +0200

    Changed wPlast to w_plast, as it was decided month ago. Idk why an old variable name is used here.

--- models/builder/templates/synapse_templates.py
+++ models/builder/templates/synapse_templates.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """This file contains dictionaries of neuron synapse or modules,
-   combined by the synapse equation builder. 
+   combined by the synapse equation builder.
 
 Modules:
     alphakernel (TYPE): Description
@@ -36,12 +36,13 @@ Modules:
 """
 from brian2 import pF, nS, mV, ms, pA, nA, volt, second
 
-############################################################################################
-#######_____TEMPLATE MODEL AND PARAMETERS_____##############################################
-############################################################################################
+##########################################################################
+#######_____TEMPLATE MODEL AND PARAMETERS_____############################
+##########################################################################
 
 
-# none model is useful when adding exponential kernel and nonplasticity at the synapse as they already present in the template model
+# none model is useful when adding exponential kernel and nonplasticity at
+# the synapse as they already present in the template model
 none = {'model': ''' ''', 'on_pre': ''' ''', 'on_post': ''' '''}
 
 
@@ -54,7 +55,7 @@ current = {'model': '''
 
             tausyne : second (constant) # synapse time constant
             tausyni : second (constant) # synapse time constant
-            wPlast : 1
+            w_plast : 1
 
             baseweight_e : amp (constant)     # synaptic gain
             baseweight_i : amp (constant)     # synaptic gain
@@ -62,18 +63,18 @@ current = {'model': '''
 
             ''',
 
-            'on_pre': '''
-            Ie_syn += baseweight_e * weight *wPlast*(weight>0)
-            Ii_syn += baseweight_i * weight *wPlast*(weight<0)
+           'on_pre': '''
+            Ie_syn += baseweight_e * weight *w_plast*(weight>0)
+            Ii_syn += baseweight_i * weight *w_plast*(weight<0)
             ''',
 
-            'on_post': ''' '''
-            }
+           'on_post': ''' '''
+           }
 
 # standard parameters for current based models
 currentPara = {"tausyne": 5 * ms,
                "tausyni": 5 * ms,
-               "wPlast": 1,
+               "w_plast": 1,
                "baseweight_e": 1 * nA,
                "baseweight_i": 1 * nA,
                "kernel_e": 0 * nA * ms**-1,
@@ -83,7 +84,7 @@ currentPara = {"tausyne": 5 * ms,
 # Additional equations for conductance based models
 conductance = {'model': '''
                dgIe/dt = (-gIe) / tausyne + kernel_e: siemens (clock-driven)
-               dgIi/dt = (-gIi) / tausyni + kernel_i : siemens (clock-driven)               
+               dgIi/dt = (-gIi) / tausyni + kernel_i : siemens (clock-driven)
 
                Ie_syn = gIe*(EIe - Vm_post) :amp
                Ii_syn = gIi*(EIi - Vm_post) :amp
@@ -96,31 +97,33 @@ conductance = {'model': '''
 
                tausyne : second (constant) # synapse time constant
                tausyni : second (constant) # synapse time constant
-               wPlast : 1
-               
+               w_plast : 1
+
                baseweight_e : siemens (constant)     # synaptic gain
                baseweight_i : siemens (constant)     # synaptic gain
                weight : 1
 
                ''',
-                
+
                'on_pre': '''
-               gIe += baseweight_e * weight *wPlast*(weight>0)
-               gIi += baseweight_i * weight *wPlast*(weight<0)
+               gIe += baseweight_e * weight *w_plast*(weight>0)
+               gIi += baseweight_i * weight *w_plast*(weight<0)
                ''',
-               
+
                'on_post': ''' '''
                }
 
 # standard parameters for conductance based models
 conductancePara = {"Ige": 0 * nS,
                    "tausyne": 5 * ms,
-                   # We define tausyn again here since its different from current base, is this a problem?
+                   # We define tausyn again here since its different from
+                   # current base, is this a problem?
                    "tausyni": 6 * ms,
                    "EIe": 60.0 * mV,
                    "EIi": -90.0 * mV,
-                   "wPlast": 1,
-                   # should we find the way to replace baseweight_e/i, since we already defined it in template?
+                   "w_plast": 1,
+                   # should we find the way to replace baseweight_e/i, since we
+                   # already defined it in template?
                    "baseweight_e": 7 * nS,
                    "baseweight_i": 3 * nS,
                    "kernel_e": 0 * nS * ms**-1,
@@ -134,7 +137,7 @@ Dpi = {'model': '''
 
 
         weight : 1
-        wPlast : 1
+        w_plast : 1
 
         Ie_gain = Io_syn*(Ie_syn<=Io_syn) + Ie_th*(Ie_syn>Io_syn) : amp
         Ii_gain = Io_syn*(Ii_syn<=Io_syn) + Ii_th*(Ii_syn>Io_syn) : amp
@@ -163,8 +166,8 @@ Dpi = {'model': '''
         Csyn         : farad (constant)
         ''',
        'on_pre': '''
-        Ie_syn += Iw_e*wPlast*Ie_gain*(weight>0)/(Itau_e*((Ie_gain/Ie_syn)+1))
-        Ii_syn += Iw_i*wPlast*Ii_gain*(weight<0)/(Itau_i*((Ii_gain/Ii_syn)+1))
+        Ie_syn += Iw_e*w_plast*Ie_gain*(weight>0)/(Itau_e*((Ie_gain/Ie_syn)+1))
+        Ii_syn += Iw_i*w_plast*Ii_gain*(weight<0)/(Itau_i*((Ii_gain/Ii_syn)+1))
         ''',
        'on_post': ''' ''',
        }
@@ -175,7 +178,7 @@ DpiPara = {
     'kn_syn': 0.75,
     'kp_syn': 0.66,
     'Ut_syn': 25. * mV,
-    "Igain": 15 * pA,
+    'Igain': 15 * pA,
     'Csyn': 1.5 * pF,
     'Ie_tau': 10. * pA,
     'Ii_tau': 10. * pA,
@@ -183,19 +186,21 @@ DpiPara = {
     'Ii_th': 10 * pA,
     'Ie_syn': 0.5 * pA,
     'Ii_syn': 0.5 * pA,
-    'wPlast': 1,
+    'w_plast': 1,
     'baseweight_e': 50. * pA,
     'baseweight_i': 50. * pA
 }
 
 
-############################################################################################
-#######_____ADDITIONAL EQUATIONS BLOCKS AND PARAMETERS_____#################################
-############################################################################################
+##########################################################################
+#######_____ADDITIONAL EQUATIONS BLOCKS AND PARAMETERS_____###############
+##########################################################################
 # Every block must specifies additional model, pre and post spike equations, as well as
-#  two different sets (dictionaries) of parameters for conductance based models or current models
+# two different sets (dictionaries) of parameters for conductance based
+# models or current models
 
-# If you want to ovverride an equation add '%' before the variable of your block's explicit equation
+# If you want to ovverride an equation add '%' before the variable of your
+# block's explicit equation
 
 # example:  Let's say we have the simplest model (current one with template equation),
 # and you're implementing a new block with this explicit equation : d{synvar_e}/dt = (-{synvar_e})**2 / synvar_e,
@@ -203,8 +208,9 @@ DpiPara = {
 # your equation will be : %d{synvar_e}/dt = (-{synvar_e})**2 / synvar_e
 
 
-########_____Plasticity Blocks_____#########################################################
-# you need to declare two set of parameters for every block : (one for current based models and one for conductance based models)
+########_____Plasticity Blocks_____#######################################
+# you need to declare two set of parameters for every block : (one for
+# current based models and one for conductance based models)
 
 # Fusi learning rule ##
 fusi = {'model': '''
@@ -235,7 +241,7 @@ fusi = {'model': '''
       down = 1. * (Vm_post<theta_V) * (Ca>theta_downl) * (Ca<theta_downh)
       w += wplus * up - wminus * down
       w = clip(w,w_min,w_max)
-      wPlast = floor(w+0.5)
+      w_plast = floor(w+0.5)
       ''',
 
         'on_post': '''Ca += w_ca'''}
@@ -287,11 +293,11 @@ stdp = {'model': '''
 
         'on_pre': '''
       Apre += dApre*w_max
-      wPlast = clip(wPlast + Apost, 0, w_max) ''',
+      w_plast = clip(w_plast + Apost, 0, w_max) ''',
 
         'on_post': '''
       Apost += -dApre * (taupre / taupost) * Q_diffAPrePost * w_max
-      wPlast = clip(wPlast + Apre, 0, w_max) '''}
+      w_plast = clip(w_plast + Apre, 0, w_max) '''}
 
 stdpPara_current = {"baseweight_e": 7 * pA,  # should we find the way to replace since we would define it twice
                     "baseweight_i": 7 * pA,
@@ -300,7 +306,7 @@ stdpPara_current = {"baseweight_e": 7 * pA,  # should we find the way to replace
                     "w_max": 1.,
                     "dApre": 0.1,
                     "Q_diffAPrePost": 1.05,
-                    "wPlast": 0}
+                    "w_plast": 0}
 
 stdpPara_conductance = {"baseweight_e": 7 * nS,  # should we find the way to replace since we would define it twice
                         "baseweight_i": 3 * nS,
@@ -309,18 +315,19 @@ stdpPara_conductance = {"baseweight_e": 7 * nS,  # should we find the way to rep
                         "w_max": 0.01,
                         "diffApre": 0.01,
                         "Q_diffAPrePost": 1.05,
-                        "wPlast": 0}
+                        "w_plast": 0}
 
-########_____Kernels Blocks_____#########################################################
-# you need to declare two set of parameters for every block : (one for current based models and one for conductance based models)
+########_____Kernels Blocks_____##########################################
+# you need to declare two set of parameters for every block : (one for
+# current based models and one for conductance based models)
 
 # TODO: THESE KERNELS ARE WRONG!
 
 # Alpha kernel ##
 
 alphakernel = {'model': '''
-             %kernel_e = baseweight_e*(weight>0)*wPlast*weight*exp(1-t_spike/tausyne_rise)/tausyne : {unit} * second **-1
-             %kernel_i = baseweight_i*(weight<0)*wPlast*weight*exp(1-t_spike/tausyni_rise)/tausyni : {unit} * second **-1
+             %kernel_e = baseweight_e*(weight>0)*w_plast*weight*exp(1-t_spike/tausyne_rise)/tausyne : {unit} * second **-1
+             %kernel_i = baseweight_i*(weight<0)*w_plast*weight*exp(1-t_spike/tausyni_rise)/tausyni : {unit} * second **-1
              dt_spike/dt = 1 : second (clock-driven)
              tausyne_rise : second
              tausyni_rise : second
@@ -347,8 +354,8 @@ alphaPara_conductance = {"tausyne": 2 * ms,
 resonantkernel = {'model': '''
                 omega: 1/second
                 sigma_gaussian : second
-                %kernel_e  = baseweight_e*(weight>0)*wPlast*(weight*exp(-t_spike/tausyne_rise)*cos(omega*t_spike))/tausyne : {unit} * second **-1
-                %kernel_i  = baseweight_i*(weight<0)*wPlast*(weight*exp(-t_spike/tausyni_rise)*cos(omega*t_spike))/tausyni : {unit} * second **-1
+                %kernel_e  = baseweight_e*(weight>0)*w_plast*(weight*exp(-t_spike/tausyne_rise)*cos(omega*t_spike))/tausyne : {unit} * second **-1
+                %kernel_i  = baseweight_i*(weight<0)*w_plast*(weight*exp(-t_spike/tausyni_rise)*cos(omega*t_spike))/tausyni : {unit} * second **-1
                 dt_spike/dt = 1 : second (clock-driven)
                 tausyne_rise : second
                 tausyni_rise : second
@@ -399,20 +406,19 @@ gaussianPara_conductance = {"sigma_gaussian_e": 6 * ms,
 nonePara = {}
 
 
-########_____Dictionary of keywords_____#########################################################
+########_____Dictionary of keywords_____##################################
 # These dictionaries contains keyword and models and parameters names useful for the __init__ subroutine
 # Every new block dictionaries must be added to these definitions
 
 modes = {'current': current, 'conductance': conductance, 'DPI': Dpi}
 
 kernels = {'exponential': none, 'alpha': alphakernel,
-                      'resonant': resonantkernel, 'gaussian': gaussiankernel}
+           'resonant': resonantkernel, 'gaussian': gaussiankernel}
 
 plasticitymodels = {'nonplastic': none, 'fusi': fusi, 'stdp': stdp}
 
 
-
-###parameters dictionaries
+# parameters dictionaries
 current_Parameters = {'current': currentPara, 'nonplastic': nonePara, 'fusi': fusiPara_current,
                       'stdp': stdpPara_current, 'exponential': nonePara, 'alpha': alphaPara_current,
                       'resonant': resonantPara_current, 'gaussian': gaussianPara_current}
@@ -421,5 +427,5 @@ conductance_Parameters = {'conductance': conductancePara, 'nonplastic': nonePara
                           'stdp': stdpPara_conductance, 'exponential': nonePara, 'alpha': alphaPara_conductance,
                           'resonant': resonantPara_conductance, 'gaussian': gaussianPara_conductance}
 
-DPI_Parameters = {'DPI': DpiPara, 'nonplastic': nonePara, 'fusi': fusiPara_current,
-                  'stdp': stdpPara_current}
+DPI_Parameters = {'DPI': DpiPara, 'exponential': nonePara, 'nonplastic': nonePara,
+                  'fusi': fusiPara_current, 'stdp': stdpPara_current}

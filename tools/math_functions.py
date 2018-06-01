4de73e12b9b4338b7e5e7083c107ad88ec6e2336
Alpha Renner <alpren@ini.uzh.ch>
2018-06-01 13:23:58 +0200

    format math_functions.py

--- tools/math_functions.py
+++ tools/math_functions.py
@@ -20,8 +20,8 @@ import numpy as np
                 }
                 ''')
 @declare_types(nrows='integer', ncols='integer', sigma_x='float', sigma_y='float', rho='float', mu_x='float', mu_y='float', result='float')
-@check_units(nrows=1, ncols=1, sigma_x=1, sigma_y=1, mu_x=1, mu_y=1, rho = 1, result=1)
-def normal2d_density(nrows, ncols, sigma_x=1, sigma_y=1, rho = 0, mu_x=None, mu_y=None):
+@check_units(nrows=1, ncols=1, sigma_x=1, sigma_y=1, mu_x=1, mu_y=1, rho=1, result=1)
+def normal2d_density(nrows, ncols, sigma_x=1, sigma_y=1, rho=0, mu_x=None, mu_y=None):
     """returns a 2d normal density distributuion array of size (nrows, ncols)
 
 
@@ -36,7 +36,7 @@ def normal2d_density(nrows, ncols, sigma_x=1, sigma_y=1, rho = 0, mu_x=None, mu_
     """
 
     x = np.arange(0, nrows)
-    y = np.reshape(np.arange(0, ncols),(ncols,1))
+    y = np.reshape(np.arange(0, ncols), (ncols, 1))
     #y = x[:, np.newaxis]
 
     if mu_x is None:
@@ -45,14 +45,15 @@ def normal2d_density(nrows, ncols, sigma_x=1, sigma_y=1, rho = 0, mu_x=None, mu_
         mu_y = ncols // 2
 
     #gaussian = np.exp(-((x - mu_x)**2 + (y - mu_y)**2) / (2 * sigma**2))
-    f1 = (1 / (2*np.pi*sigma_x*sigma_y*np.sqrt(1-rho**2)))
-    f2 = -(1 / (2*(1-rho**2)))
-    fx = (x - mu_x)/sigma_x
-    fy = (y - mu_y)/sigma_y
-    fxy = 2*fx*fy*rho
-    density = f1*np.exp(f2*(fx**2+fy**2-fxy))
+    f1 = (1 / (2 * np.pi * sigma_x * sigma_y * np.sqrt(1 - rho**2)))
+    f2 = -(1 / (2 * (1 - rho**2)))
+    fx = (x - mu_x) / sigma_x
+    fy = (y - mu_y) / sigma_y
+    fxy = 2 * fx * fy * rho
+    density = f1 * np.exp(f2 * (fx**2 + fy**2 - fxy))
 
     return density
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -60,5 +61,5 @@ if __name__ == '__main__':
     plt.figure()
     plt.imshow(img)
     plt.colorbar()
-    #the sum is almost one, if the sigmas are not much larger than
+    # the sum is almost one, if the sigmas are not much larger than
     print(np.sum(img))

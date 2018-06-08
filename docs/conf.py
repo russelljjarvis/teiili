c3214a71d5ec6251d2d439976d325edda207c6ab
Moritz Milde <mmilde@ini.uzh.ch>
2018-06-08 14:15:31 +0200

    Add numpy style docstrings support

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,10 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
 ]
-
+autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
 
@@ -64,7 +65,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'Python'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

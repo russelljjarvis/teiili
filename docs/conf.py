3cf9d8551aeb35e1c2ca22fcf75fa2cbcf84767b
mmilde <mmilde@ini.uzh.ch>
2018-06-15 10:53:37 +0200

    Change path to source files

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath("../../"))
+# sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../core/"))
+sys.path.insert(0, os.path.abspath("../building_blocks/"))
 
 
 # -- Project information -----------------------------------------------------

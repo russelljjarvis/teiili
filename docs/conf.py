2c7b3b10820ee8e5bb3507ad2c15c13c9c6d3045
Moritz Milde <mmilde@ini.uzh.ch>
2018-06-20 09:05:46 +0200

    Update mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
 # 'matplotlib.colors', 'pyqtgraph.colormap', 'pyqtgraph',
 # 'pyqtgraph.Qt',
 # 'pyqtgraph.exporters', 'itertools', 'sparse', 'tkinter'
-MOCK_MODULES = ['brian2.figure']
+MOCK_MODULES = ['numpy', 'matplotlib', 'scipy']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- Project information -----------------------------------------------------

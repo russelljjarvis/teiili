b18622f2eec59283514a802ae221e98fd2650752
Moritz Milde <mmilde@ini.uzh.ch>
2018-06-20 17:22:44 +0200

    Add Group to Neurons and Connections.

--- teili/core/groups.py
+++ teili/core/groups.py
@@ -126,7 +126,7 @@ class TeiliGroup(Group):
         return self.equation_builder.keywords['model']
 
 
-class Neurons(NeuronGroup, TeiliGroup):
+class Neurons(Group, NeuronGroup, TeiliGroup):
     """This class is a subclass of NeuronGroup.
 
     You can use it as a NeuronGroup, and everything will be passed to NeuronGroup.
@@ -263,7 +263,7 @@ class Neurons(NeuronGroup, TeiliGroup):
         return TeiliSubgroup(self, start, stop)
 
 
-class Connections(Synapses, TeiliGroup):
+class Connections(Group, Synapses, TeiliGroup):
     """This class is a subclass of Synapses.
 
     You can use it as a Synapses, and everything will be passed to Synapses.

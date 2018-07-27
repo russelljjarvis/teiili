bf965db00f07e4a4c63144f90f8b83686fcd5732
Nicoletta Risi <nicoletta@ini.uzh.ch>
2018-07-27 13:23:06 +0200

    Update the tests for the mismatch

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -19,47 +19,56 @@ class TestGroups(unittest.TestCase):
         np.random.seed(seed_)
         seed(seed_)
         
-    def test_add_mismatch(self):
-        """
-        This method tests the method add_mismatch()
-        """
-        self._test_mismatch_added()
-        self._test_mismatch_lower_bound()
-        self._test_mismatch_seed()
-        
-    def _test_mismatch_added(self):
+    def test_mismatch_added(self):
         """
         This tests if the function add_mismatch() changes the selected parameter.
         This tests also NameError (raised when the parameter is not included in the model) 
         and UserWarning (raised if the function is called twice, which means 
-        adding the mismatch twice without resetting parameters).
+        adding the mismatch twice without resetting parameters) 
         """
-        tetsNeurons1 = Neurons(2, equation_builder=DPI(num_inputs=2), name="testNeuron1")          
-        param = 'Itau'
-        old_param_value = np.copy(getattr(tetsNeurons1, param))
-        tetsNeurons1.add_mismatch({param: 0.1})
+        testNeurons = Neurons(100, equation_builder=DPI(num_inputs=2))          
+        old_param_value = np.copy(getattr(testNeurons, 'Itau'))
         
-        self.assertFalse(any(old_param_value == np.asarray(getattr(tetsNeurons1, param))))
+        testNeurons.add_mismatch({'Itau': 0.1}, seed=10)        
+        self.assertFalse(any(old_param_value == np.asarray(getattr(testNeurons, 'Itau'))))
         
+        # Adding mismatch twice:
+        testNeurons.add_mismatch({'Itau': 0.1})          
+        self.assertWarns(UserWarning, testNeurons.add_mismatch({'Itau': 0.1}))
+        
+        # Trying to add mismatch to one parameter not included in the neuron model
         with self.assertRaises(NameError):
-            tetsNeurons1.add_mismatch({'Cm': 0.1})
-        
-        tetsNeurons1.add_mismatch({param: 0.1})        
-        
-        self.assertWarns(UserWarning, tetsNeurons1.add_mismatch({param: 0.1}))
-    
-    def _test_mismatch_lower_bound(self):
+            testNeurons.add_mismatch({'Vm': 0.1})
+
+    def test_mismatch_lower_bound(self):
         """
-        This checks that mismatch added does not lower the parameter below zero.
+        This tests the lower bound of the gaussian distribution using directly
+        the internal function _add_mismatch_param() to add mismatch to one single 
+        parameter. If not set explicitely, the lower bound should be zero. 
+        Otherwise, the function raises a warning if the lower bound goes below zero.
         """
-        tetsNeurons1 = Neurons(2, equation_builder=DPI(num_inputs=2), name="testNeuron1")          
-        param = 'Itau'
-        old_param_value = np.copy(getattr(tetsNeurons1, param))
-        tetsNeurons1.add_mismatch({param: 0.1})
+        testNeurons = Neurons(100, equation_builder=DPI(num_inputs=2))                  
         
-        self.assertTrue(all(new_param_value > 0 for new_param_value in old_param_value))
+        # Test default value of lower bound:
+        testNeurons._add_mismatch_param(param='Itau', std=0.1)
+        expected_lower = 0
+        self.assertTrue(all(new_param_value > expected_lower for new_param_value 
+                            in getattr(testNeurons, 'Itau')))
         
-    def _test_mismatch_seed(self):
+        # Test input lower bound:
+        param = 'Iath'
+        lower = -0.2
+        std = 0.1
+        old_param = getattr(testNeurons, param)[0]
+        testNeurons._add_mismatch_param(param, std, lower)
+        expected_lower = lower * std * old_param + old_param     
+        self.assertTrue(all(new_param_value > expected_lower for new_param_value in getattr(testNeurons, param)))
+
+        # Negative lower bound:
+        with self.assertWarns(UserWarning):
+            testNeurons._add_mismatch_param(param='Ith', std=0.1, lower=-11)
+        
+    def test_mismatch_seed(self):
         """
         This method tests that the random number generation inside the mismatch 
         function does not change the current internal state of the random generator.
@@ -68,27 +77,11 @@ class TestGroups(unittest.TestCase):
         """
         
         np_current_state = np.random.get_state()
-        tetsNeurons1 = Neurons(2, equation_builder=DPI(num_inputs=2), name="testNeuron1")          
+        testNeurons = Neurons(2, equation_builder=DPI(num_inputs=2))          
         param = 'Itau'
-        tetsNeurons1.add_mismatch({param: 0.1})
+        testNeurons.add_mismatch({param: 0.1})
         
         self.assertTrue(all(np_current_state[1] == np.random.get_state()[1]))
         
-    def test__set_mismatch(self):
-        """
-        This method tests the method _set_mismatch() when a lower bound for the
-        standard normal distribution of the mismatch is set as a multiple of the
-        standard deviation (std), specified as a fraction of the parameter current value.
-        """
-        tetsNeurons1 = Neurons(2, equation_builder=DPI(num_inputs=2), name="testNeuron1")          
-        param = 'Itau'
-        std = 0.1
-        lower = 2
-        old_param = np.unique(getattr(tetsNeurons1, param))
-        expected_lower_value = -(lower * std * old_param + old_param)
-        tetsNeurons1._set_mismatch(param, std=0.1, lower=lower)
-        
-        self.assertTrue(all(new_param_value > expected_lower_value for new_param_value in getattr(tetsNeurons1, param) ))
-    
 if __name__=='__main__':
     unittest.main(verbosity=1)    

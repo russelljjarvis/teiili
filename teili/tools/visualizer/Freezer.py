651eec6438b8da27aa57922b3c9dae2c3122ffab
Renate Krause <rekrau@ini.ethz.ch>
2018-08-06 11:27:18 +0200

    Add class to freeze class attributes

---
+++ teili/tools/visualizer/Freezer.py
@@ -0,0 +1,12 @@
+
+class Freezer(object):
+    ''' class to freeze a class so that no new attributes can be added after self._freeze() is called '''
+    __isfrozen = False
+
+    def __setattr__(self, key, value):
+        if self.__isfrozen and not hasattr(self, key):
+            raise TypeError("%r is a frozen class" % self)
+        object.__setattr__(self, key, value)
+
+    def _freeze(self):
+        self.__isfrozen = True

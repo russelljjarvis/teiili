d7a8543d17e6c26f833f655144461f6ea026d44a
mmilde <mmilde@ini.uzh.ch>
2018-08-08 15:43:15 +0200

    Remove assignment of _init_parameters
    Erronously introduced by sergio, to load brian2-like models using the Neurons.

--- teili/core/groups.py
+++ teili/core/groups.py
@@ -355,7 +355,6 @@ class Neurons(NeuronGroup, TeiliGroup):
         self.num_synapses = 0
         self.synapses_dict = {}
         self.parameters = parameters
-        self._init_parameters = parameters
 
         if equation_builder is not None:
             # if inspect.isclass(equation_builder):

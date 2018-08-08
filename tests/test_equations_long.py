0973393fcc3a993f1e84b0e3c8b4471359204a97
mmilde <mmilde@ini.uzh.ch>
2018-08-08 16:11:14 +0200

    Remove prints and plots from test
    This is not a real test, as nothing is asserted. Sergio please fix this ASAP.
    I removed the equation prints and the plotting function!

--- tests/test_equations_long.py
+++ tests/test_equations_long.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Moritz Milde
 # @Date:   2017-12-17 13:06:18
-# @Last Modified by:   Moritz Milde
-# @Last Modified time: 2018-06-11 18:19:14
+# @Last Modified by:   mmilde
+# @Last Modified time: 2018-08-08 15:57:30
 """
 This file contains unittest for equations
 """
@@ -19,6 +19,7 @@ prefs.codegen.target = "numpy"
 from brian2 import prefs, ms, pA, nA, StateMonitor, device, set_device,\
     second, msecond, defaultclock, TimedArray, mV, pfarad
 pF = pfarad
+
 
 class TestEquations(unittest.TestCase):
 
@@ -48,24 +49,24 @@ class TestEquations(unittest.TestCase):
             num_inputs=1), name="testNeuron", verbose=False)
         Net = teiliNetwork()
         I_bias = 1000 * pA + 500 * pA
-        testNeurons.namespace.update({'I_bias':I_bias})
-        testNeurons.run_regularly("Iconst = I_bias",dt=1*ms)
+        testNeurons.namespace.update({'I_bias': I_bias})
+        testNeurons.run_regularly("Iconst = I_bias", dt=1 * ms)
 
         statemon_izh = StateMonitor(testNeurons,
-                           ('Ie0', 'Ii0','Iconst','Vm','Iadapt'),
-                           record=True,
-                           name='statemon_izh')
+                                    ('Ie0', 'Ii0', 'Iconst', 'Vm', 'Iadapt'),
+                                    record=True,
+                                    name='statemon_izh')
         Net.add(testNeurons, statemon_izh)
         testNeurons.Vm = -60 * mV
         testNeurons.Cm = 250 * pF
-        testNeurons.print_equations()
+        # testNeurons.print_equations()
         Net.run(1e3 * ms)
 
-        for var in [statemon_izh.Vm.T, statemon_izh.Iadapt.T]:
-            plt.figure()
-            plt.plot(statemon_izh.t/ms, var)
-        plt.show(0)
-        # import ipdb; ipdb.set_trace()
-        
+        # for var in [statemon_izh.Vm.T, statemon_izh.Iadapt.T]:
+        #     plt.figure()
+        #     plt.plot(statemon_izh.t / ms, var)
+        # plt.show(0)
+
+
 if __name__ == '__main__':
     unittest.main()

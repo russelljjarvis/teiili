37612e1684217d4804c9b5bb4c2970fce01544fa
Renate Krause <rekrau@ini.ethz.ch>
2018-08-09 18:24:45 +0200

    Add classmethods to DataModel to save and restore DataModels to/from file

--- teili/tools/visualizer/DataModels/DataModel.py
+++ teili/tools/visualizer/DataModels/DataModel.py
@@ -1,3 +1,31 @@
+try:
+    import _pickle as pickle
+except BaseException:
+    import cPickle as pickle
+
+
 class DataModel(object):
     """ Parent class of all DataModels """
-    pass
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_file(cls, path_to_file):
+        """ Classmethod to initialize DataModel object from DataModel object stored in pickle-file at path_to_file"""
+        newDataModel = cls()
+        newDataModel.load_datamodel(path_to_file)
+        return newDataModel
+
+    def save_datamodel(self, outputfilename):
+        """ Save DataModel object to outputfilename with pickle"""
+        with open(outputfilename + '.pkl', 'wb') as f:
+            pickle.dump(self, f)
+
+    def load_datamodel(self, path_to_data):
+        """ Load DataModel instance from path_to_data """
+        with open(path_to_data + '.pkl', 'rb') as f:
+            data = pickle.load(f)
+
+        for varible_name, variable_values in vars(data).items():
+            setattr(self, varible_name, variable_values)

aae6e1280e625895a6571d1f8f1cdb326b6bae41
Renate Krause <rekrau@ini.ethz.ch>
2018-08-17 12:55:31 +0200

    Improve save_plot functions for pyqtgraph backend
    Make figure_size imput optional
    Add second exporter (Image exporter) which allows also export as png, tiff and jpg
    Add to comments which output formats are supported

--- teili/tools/visualizer/DataViewers/RasterplotViewerPyqtgraph.py
+++ teili/tools/visualizer/DataViewers/RasterplotViewerPyqtgraph.py
@@ -211,16 +211,20 @@ class RasterplotViewerPyqtgraph(RasterplotViewer):
     def save_rasterplot(
         self,
         path_to_save='rasterplot.svg',
-        figure_size=(
-            2000,
-            1000)):
-        """ Save figure to path_to_save with size figure_size as svg
+        figure_size=None):
+        """ Save figure to path_to_save with size figure_size as svg, png, jpg and tiff
         Args:
             path_to_save (str): path to location where to save figure incl filename
-            figure_size (2-tuple): tuple of width and height of figure to save
+            figure_size (tuple): tuple of width and height of figure to save
         """
         self.QtApp.processEvents()
-        self.mainfig.resize(figure_size[0], figure_size[1])
-        ex = pg.exporters.SVGExporter(self.mainfig.scene())
+        if figure_size is not None:
+            self.mainfig.resize(figure_size[0], figure_size[1])
+
+        if path_to_save.split('.')[-1] == 'svg':
+            ex = pg.exporters.SVGExporter(self.mainfig.scene())
+        else:
+            ex = pg.exporters.ImageExporter(self.mainfig.scene())
+
         ex.export(fileName=path_to_save)
         print('Figure saved to: ' + path_to_save)

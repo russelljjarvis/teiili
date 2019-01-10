585a07651218613dc1837dcc827a89bfe59c22e8
Alpha Renner <alpren@ini.uzh.ch>
2019-01-10 17:12:12 +0100

    fix live plot
    live plot broke when we changed all state variables to be vectors instead of scalars, now, I show in the demo how to create variables that are changable in the gui and that directly influence other state variables that can be vectors

--- examples/wta_live_demo.py
+++ examples/wta_live_demo.py
@@ -7,26 +7,33 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
-
+import sys
 import scipy
 from scipy.optimize import minimize
 from scipy import ndimage
+from pyqtgraph.Qt import QtGui #, QtCore
 
 from brian2 import prefs, ms, pA, nA, StateMonitor, device, set_device,\
  second, msecond, defaultclock
 
 
-from teili.building_blocks.wta import WTA, plotWTA
+from teili.building_blocks.wta import WTA
 from teili.core.groups import Neurons, Connections
 from teili.stimuli.testbench import WTA_Testbench
-from teili import teiliNetwork
+from teili import TeiliNetwork
 from teili.models.synapse_models import DPISyn, DPIstdp
 from teili.tools.synaptic_kernel import kernel_gauss_1d
+from teili.tools.live import ParameterGUI, PlotGUI
+
 
 from teili import NeuronEquationBuilder, SynapseEquationBuilder
 DPI = NeuronEquationBuilder.import_eq('DPI', num_inputs=1)
 
-from teili.tools.live import ParameterGUI, PlotGUI
+app = QtGui.QApplication.instance()
+if app is None:
+    app = QtGui.QApplication(sys.argv)
+else:
+    print('QApplication instance already exists: %s' % str(app))
 
 prefs.codegen.target = 'numpy'
 
@@ -37,8 +44,8 @@ num_input_neurons = num_neurons
 
 x = np.arange(0,num_neurons-1,1)
 
-Net = teiliNetwork()
-duration = 500#10000
+Net = TeiliNetwork()
+duration = 1500#10000
 testbench = WTA_Testbench()
 
 wtaParams = {'weInpWTA': 500,
@@ -61,51 +68,45 @@ syn_ex_ex = gtestWTA.Groups['synWTAWTA1e']
 syn_ex_ih = gtestWTA.Groups['synWTAInh1e']
 syn_ih_ex = gtestWTA.Groups['synInhWTA1i']
 
-testbench.stimuli(num_neurons=num_neurons, dimensions=1, start_time=100, end_time=duration)
+testbench.stimuli(num_neurons=num_neurons-1, dimensions=1, start_time=100, end_time=duration)
 testbench.background_noise(num_neurons=num_neurons, rate=10)
 
-#gtestWTA.inputGroup.set_spikes(indices=testbench.indices, times=testbench.times * ms)
+gtestWTA.inputGroup.set_spikes(indices=testbench.indices, times=testbench.times * ms)
 noise_syn = Connections(testbench.noise_input, gtestWTA,
                         equation_builder=DPISyn(), name="noise_syn",)
 noise_syn.connect("i==j")
 noise_syn.weight = 500
-
 statemonWTAin = StateMonitor(gtestWTA.Groups['gWTAGroup'], ('Ie0', 'Ii0','Ie1', 'Ii1','Ie2', 'Ii2','Ie3', 'Ii3'), record=True,
                                        name='statemonWTAin')
 
+
+# We have to add shared (scalar) parameters that are influencing the other parameters if we want to change them via the GUI
+#gtestWTA.Groups['gWTAGroup'].add_state_variable('', unit=1, shared=True, constant=False, changeInStandalone=False)
+syn_in_ex.add_state_variable('guiweight', unit=1, shared=True, constant=False, changeInStandalone=False)
+syn_in_ex.guiweight = 400
+syn_in_ex.run_regularly("weight = guiweight", dt=1*ms)
+
+noise_syn.add_state_variable('guiweight', unit=1, shared=True, constant=False, changeInStandalone=False)
+noise_syn.guiweight = 400
+noise_syn.run_regularly("weight = guiweight", dt=1*ms)
+
 Net.add(gtestWTA, testbench.noise_input, noise_syn, statemonWTAin)
 
-
 #%%
-
 duration= 2000* ms
 
 plot_gui = PlotGUI(data = gtestWTA.group.Imem)
 
 param_gui = ParameterGUI(net = Net)
 param_gui.params
-param_gui.add_params(parameters = [gtestWTA.group.Cahp,gtestWTA.group.Ithahp])
+param_gui.add_params(parameters = [syn_in_ex.guiweight, noise_syn.guiweight])
 param_gui.showGUI()
 
+#%%
 Net.run_as_thread(duration=duration)
 
-#gtestWTA.group.Ithahp
-#gtestWTA.group.Cahp
+# syn_in_ex.guiweight
+# syn_in_ex.weight
 
+app.exec_()
 
-#wta_plot = plotWTA(wta_monitors=gtestWTA.Monitors, name='testWTA',
-#                     start_time=0 * ms, end_time=duration, plot_states=False)
-#wta_plot.show()
-#
-#spikemonWTA = gtestWTA.Groups['spikemonWTA']
-#spiketimes = spikemonWTA.t
-#dt = defaultclock.dt
-#spikeinds = spiketimes/dt
-#
-#data_sparse = scipy.sparse.coo_matrix((np.ones(len(spikeinds)),(spikeinds,[i for i in spikemonWTA.i])))
-#data_dense = data_sparse.todense()
-#
-##data_dense.shape
-#filtersize = 500*ms
-#data_filtered = ndimage.uniform_filter1d(data_dense, size=int(filtersize / dt), axis=0, mode='constant') * second / dt
-#plt.plot(data_filtered[-10]) #[400,:])

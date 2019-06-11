7bf59be65a2a70892acb497c5fa2dbe36877295b
Renate Krause <renate.b.krause@bluewin.ch>
2019-06-11 23:02:27 +0200

    Factor out run_brian_netowork from unittest

--- tests/test_EventsModel.py
+++ tests/test_EventsModel.py
@@ -1,88 +1,7 @@
 import unittest
 
-import numpy as np
-
-from brian2 import us, ms, prefs, defaultclock, start_scope, SpikeGeneratorGroup, SpikeMonitor, StateMonitor
-
-from teili.core.groups import Neurons, Connections
-from teili import TeiliNetwork
-from teili.models.neuron_models import DPI
-from teili.models.synapse_models import DPISyn
-from teili.models.parameters.dpi_neuron_param import parameters as neuron_model_param
-
 from teili.tools.visualizer.DataModels import EventsModel
-
-
-def run_teili_network():
-
-    prefs.codegen.target = "numpy"
-    defaultclock.dt = 10 * us
-
-    start_scope()
-
-    # parameters
-    N_input, N_N1, N_N2 = 1, 5, 3
-    duration_sim = 150
-
-    Net = TeiliNetwork()
-    # setup spike generator
-    spikegen_spike_times = np.sort(
-        np.random.choice(
-            size=30,
-            a=range(
-                0,
-                duration_sim,
-                5),
-            replace=False)) * ms
-    spikegen_neuron_ids = np.zeros_like(spikegen_spike_times) / ms
-    gInpGroup = SpikeGeneratorGroup(
-        N_input,
-        indices=spikegen_neuron_ids,
-        times=spikegen_spike_times,
-        name='gtestInp')
-    # setup neurons
-    testNeurons1 = Neurons(
-        N_N1, equation_builder=DPI(
-            num_inputs=2), name="testNeuron")
-    testNeurons1.set_params(neuron_model_param)
-    testNeurons2 = Neurons(
-        N_N2, equation_builder=DPI(
-            num_inputs=2), name="testNeuron2")
-    testNeurons2.set_params(neuron_model_param)
-    # setup connections
-    InpSyn = Connections(
-        gInpGroup,
-        testNeurons1,
-        equation_builder=DPISyn(),
-        name="testSyn",
-        verbose=False)
-    InpSyn.connect(True)
-    InpSyn.weight = '100 + rand() * 50'
-    Syn = Connections(
-        testNeurons1,
-        testNeurons2,
-        equation_builder=DPISyn(),
-        name="testSyn2")
-    Syn.connect(True)
-    Syn.weight = '100+ rand() * 50'
-    # spike monitors input and network
-    spikemonN1 = SpikeMonitor(testNeurons1, name='spikemon')
-    # # state monitor neurons
-    statemonN1 = StateMonitor(
-        testNeurons1, variables=[
-            "Iin", "Imem"], record=[
-            0, 3], name='statemonNeu')
-
-    Net.add(gInpGroup, testNeurons1, testNeurons2,
-            InpSyn, Syn,
-            spikemonN1,
-            statemonN1,
-            )
-    Net.run(duration_sim * ms)
-    print('Simulation run for {} ms'.format(duration_sim))
-
-    return Net, spikemonN1, statemonN1
-
+from utils_unittests import run_brian_network
 
 class TestEventsModel(unittest.TestCase):
 
@@ -96,7 +15,7 @@ class TestEventsModel(unittest.TestCase):
         self.assertTrue(len(EM.spike_times) == len(spike_times))
 
     def test_EventsModel_from_brian_spike_monitor(self):
-        Net, spikemonN1, statemonN1 = run_teili_network()
+        spikemonN1, _, statemonN1, _ = run_brian_network()
         EM = EventsModel.from_brian_spike_monitor(spikemonN1)
 
         self.assertTrue(len(EM.neuron_ids) == len(spikemonN1.i))

5138c072abdd68d3fb028bcebfbd3eed1abe218b
Renate Krause <rekrau@ini.ethz.ch>
2019-06-21 11:40:48 +0200

    Remove pickle from docstrings/comments after changing save/load datamodel methods to use npz

--- teili/tools/visualizer/DataModels/DataModel.py
+++ teili/tools/visualizer/DataModels/DataModel.py
@@ -11,13 +11,13 @@ class DataModel(object):
     @classmethod
     def from_file(cls, path_to_file):
         """ Classmethod to initialize DataModel object from DataModel object
-            stored in pickle-file at path_to_file """
+            stored in npz-file at path_to_file """
         newDataModel = cls()
         newDataModel.load_datamodel(path_to_file)
         return newDataModel
 
     def save_datamodel(self, outputfilename):
-        """ Save DataModel object to outputfilename with pickle"""
+        """ Save DataModel object to outputfilename with npz"""
         arrays_to_save_by_name = {}
         for attr_name in self.attributes_to_save:
             arrays_to_save_by_name[attr_name] = getattr(self, attr_name)
@@ -25,7 +25,7 @@ class DataModel(object):
 
     def load_datamodel(self, path_to_data):
         """ Load DataModel instance from path_to_data """
-        data = np.load(path_to_data, allow_pickle=True)
+        data = np.load(path_to_data)
         for varible_name, variable_values in data.items():
             setattr(self, varible_name, variable_values)
         data.close()

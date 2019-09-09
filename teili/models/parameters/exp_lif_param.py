d7dc4092745a35a9a63b59faec2106663bc94494
mmilde <mmilde@ini.uzh.ch>
2019-09-09 13:43:02 +1000

    Adds two new sets of parameter dictioanry.

---
+++ teili/models/parameters/exp_lif_param.py
@@ -0,0 +1,10 @@
+from brian2 import pF, nS, mV, ms, pA, nA
+
+parameters = {"Cm": 281 * pF,
+              "refP": 2 * ms,
+              "Iadapt": 0 * pA,
+              "Inoise": 0 * pA,
+              "Iconst": 0 * pA,
+              "Vthr": -50.4 * mV,
+              "Vres": -70.6 * mV
+              }
---
+++ teili/models/parameters/linear_lif_param.py
@@ -0,0 +1,11 @@
+from brian2 import pF, nS, mV, ms, pA, nA
+
+parameters = {"Cm": 281 * pF,
+              "refP": 2 * ms,
+              "Iexp": 0 * pA,
+              "Iadapt": 0 * pA,
+              "Inoise": 0 * pA,
+              "Iconst": 0 * pA,
+              "Vthr": -50.4 * mV,
+              "Vres": -70.6 * mV
+              }

ae7ace755162aac2c77dd381ea5aae70c7a94c1a
mmilde <mmilde@ini.uzh.ch>
2019-12-04 12:10:31 +1100

    Refactors code and adds re_init_tau run_regularly.

--- teili/tools/run_regularly/run_reg_functions.py
+++ teili/tools/run_regularly/run_reg_functions.py
@@ -1,46 +1,8 @@
 # -*- coding: utf-8 -*-
-"""To test different learning strategies and calculate proxy parameters we developed
-different run_regularly functions to track the weights and update them.
-The functions are all currently in numpy, but potentially can be converted into cpp.
+""" A collection of run_regular functions including weight_initilization,
+weight_normalization, adaptive thresholds etc.
 
-These function include:
-    re-initializing of weights
-    If the summed weight per post-synatpic neuron is below a user defined threshold.
-
-    weight regularization:
-    As in conventional ANN's we need regularizer on the weights to ensure stability.
-        L1 norm:
-        The idea of L1 norm is favour a few weights, which has the consequence of
-        most weights being zero and a few weights are large, e.g. 1.
-
-        L2 norm:
-        The ideao of L2 norm is to have many small weights. In standard neural networks
-        synapses can be both, excitatory and inhibitory, which means that weights tend to be centered
-        around 0 if L2 regularization is used. In the spiking neural network case synapses are either
-        inhibitory or excitatory, which has the consequence that the weights tend to be centered
-        around the mean, which is non-zero, e.g. 0.3.
-
-    Correlation tracking
-    Correlation tracking is used to determine if L1 or L2 weight regularization should be used.
-    The idea is to estimate the slope of the cross-correlation coefficient for each post-synaptic
-    neuron in a  small time window, which is set by the buffer. If the slope tends to become zero
-    the weight regularization switches from L2 (exploration) to L1 (facilitation).
-
-    Membrane variance
-    The membrane variance is used to determine if the the inhibitory weight should be increased or decreased.
-    The intuition behind this is that the, since neurons are recurrently connected each post-syaptic membrane
-    variance signals a high correlated activity. In order to diversify the firing pattern inh. synapses connected
-    to neurons with a high membrane variance is increased, lower their variance. On the contrary, if the membrane
-    variance is to low, the inhibitory weights should be decreased to allow those neurons to pick up uncovered
-    input statistics.
-
-    normalization of the membrane variance
-    The normalization is used to make the comparison between neurons easier and set a threshold independent
-    of the unit and between 0 and 1.
-
-Example:
-
-
+For more details see below.
 """
 # @Author: Moritz Milde
 # @Date:   2018-06-22 10:05:24
@@ -50,8 +12,23 @@ from brian2 import implementation, check_units, amp, pA
 
 
 @implementation('numpy', discard_units=True)
-@check_units(weights=1, source_N=1, target_N=1, re_init_threshold=1, dist_param=1, scale=1, dist=1, result=1)
-def re_init_weights(weights, source_N, target_N, re_init_threshold=0.2, dist_param=0.4, scale=0.2, dist=0):
+@check_units(weights=1,
+             source_N=1,
+             target_N=1,
+             re_init_index=1,
+             re_init_threshold=1,
+             dist_param=1,
+             scale=1,
+             dist=1,
+             result=1)
+def re_init_weights(weights,
+                    source_N,
+                    target_N,
+                    re_init_index=None,
+                    re_init_threshold=0.2,
+                    dist_param=0.4,
+                    scale=0.2,
+                    dist=0):
     """Re-initializes a given weight matrix using a normal distribution with
     a specified mean and standard deviation if the mean is below a user
     defined threshold.
@@ -97,6 +74,10 @@ def re_init_weights(weights, source_N, target_N, re_init_threshold=0.2, dist_par
         weights (np.ndarray. required): Flattened weight matrix.
         source_N (int, required): Number of units in the source population.
         target_N (int, required): Number of units in the target population.
+        re_init_index (array, bool, optional): Boolean index array indicating
+            which weights need to be re-initilized. If None weights are updated
+            based on average post-synaptic weight. re_init_index can be optained
+            using get_re_init_index run_regularly.
         re_init_threshold (float, optional): Re-initialization threshold.
         dist_param (float, optional): Shape factor of gamma, or mean of
             normal distribution from which weights are sampled.
@@ -109,8 +90,9 @@ def re_init_weights(weights, source_N, target_N, re_init_threshold=0.2, dist_par
     data = np.reshape(weights, (source_N, target_N))
 
     # Thresholding post-synaptic weights
-    reinit_index = np.logical_or(np.mean(data, 0) < re_init_threshold,
-                                 np.mean(data, 0) > (1 - re_init_threshold))
+    if re_init_index is None:
+        reinit_index = np.logical_or(np.mean(data, 0) < re_init_threshold,
+                                    np.mean(data, 0) > (1 - re_init_threshold))
 
     # Re-initializing weights with normal distribution
     if dist == 1:
@@ -123,6 +105,122 @@ def re_init_weights(weights, source_N, target_N, re_init_threshold=0.2, dist_par
                                            (source_N, np.sum(reinit_index)))
     data = np.clip(data, 0, 1)
     return data.flatten()
+
+
+@implementation('numpy', discard_units=True)
+@check_units(taus=second,
+             re_init_index=1,
+             source_N=1,
+             target_N=1,
+             dist_param=1,
+             scale=1,
+             dist=1,
+             result=second)
+def re_init_taus(taus,
+                 re_init_index,
+                 source_N, target_N,
+                 dist_param=0.4,
+                 scale=0.2,
+                 dist=0):
+    """Re-initializes a given synaptic time constants matrix using a normal
+    distribution with a specified mean and standard deviation given
+    a re_init_index vector.
+
+    Args:
+        taus (np.ndarray. required): Flattened synaptic time constant matrix.
+        re_init_index (np.ndarray, required): Boolean array of rows to be updated in matrix
+        source_N (int, required): Number of units in the source population.
+        target_N (int, required): Number of units in the target population.
+        dist_param (float, optional): Shape factor of gamma, or mean of
+            normal distribution from which weights are sampled.
+        scale (float, optional): Scale factor of gamma distribution from which weights are sampled.
+
+    Returns:
+        ndarray: Flatten re-initialized tau matrix
+    """
+    data = np.zeros((source_N, target_N)) * np.nan
+    data = np.reshape(taus / ms, (source_N, target_N))
+    # Re-initializing weights with normal distribution
+    if dist == 1:
+        data[:, re_init_index] = np.reshape(np.random.gamma(shape=dist_param, scale=scale,
+                                                            size=source_N * np.sum(re_init_index)),
+                                            (source_N, np.sum(re_init_index)))
+    if dist == 0:
+        data[:, re_init_index] = np.reshape(np.random.normal(loc=dist_param, scale=scale,
+                                                             size=source_N * np.sum(re_init_index)),
+                                            (source_N, np.sum(re_init_index)))
+    data = np.clip(data, 0.05, 200)
+    return data.flatten() * ms
+
+
+@implementation('numpy', discard_units=True)
+@check_units(Imem=volt, buffer_pointer=1, membrane_buffer=1, kernel=1, result=volt)
+def get_activity_proxy_vm(Vm, buffer_pointer, membrane_buffer, kernel):
+    """This function calculates an activity proxy using an integrated, exponentially
+    weighted estimate of Imem of the N last time steps and stores it in membrane_buffer.
+
+    This is needed for Variance Dependent Plasticity of inhibitory
+    synapse.
+
+    Example:
+        >>> from teili.core.groups import Neurons, Connections
+        >>> from teili.models.builder.neuron_equation_builder import NeuronEquationBuilder
+        >>> from teili.models.builder.synapse_equation_builder import SynapseEquationBuilder
+
+        >>> LIFvar = NeuronEquationBuilder.import_eq('octa/models/equations/LIFvar',
+                                                         num_inputs=2)
+        >>> Alphavdp = SynapseEquationBuilder.import_eq('octa/models/equations/Alphavdp')
+
+        >>> neuron_obj = Neurons(2, equation_builder=LIFvar,
+                                 name="neuron_obj")
+        >>> syn_obj = Connections(neuron_obj,
+                                  neuron_obj,
+                                  equation_builder=Alphavdp,
+                                  method='euler',
+                                  name='syn_obj')
+
+        >>> neuron_obj.namespace.update({'get_membrane_variance': get_membrane_variance})
+        >>> neuron_obj.namespace.update({'normalize_membrane_variance': normalize_membrane_variance})
+        >>> neuron_obj.add_state_variable('buffer_size', shared=True, constant=True)
+        >>> neuron_obj.add_state_variable('buffer_pointer', shared=True, constant=True)
+        >>> neuron_obj.namespace.update({'decay': decay})
+
+        >>> neuron_obj.buffer_size = octaParams['buffer_size_plast']
+        >>> neuron_obj.buffer_pointer = -1
+        >>> neuron_obj.variables.add_array('membrane_buffer', size=(neuron_obj.N,
+                                        octaParams['buffer_size_plast']))
+        >>> # Now we can initialize the buffer_pointer and add the run regularly function
+        >>> neuron_obj.run_regularly('''buffer_pointer = (buffer_pointer + 1) % buffer_size
+            activity_proxy = get_activity_proxy(Vm, buffer_pointer, membrane_buffer, decay)''', dt=1*ms)
+
+    Args:
+        Vm (float): The membrane potential of the LIF neuron model.
+        buffer_pointer (int): Pointer to keep track of the buffer
+        membrane_buffer (numpy.ndarray): Ring buffer for membrane potential
+        decay (int): Time constant of exp. weighted decay
+
+    Returns:
+        neuron_obj.array: An array which holds the integral of the
+            activity_proxy over the N time steps of the
+    """
+    buffer_pointer = int(buffer_pointer)
+    if np.sum(membrane_buffer == 0) > 0:
+        membrane_buffer[:, buffer_pointer] = Vm
+        # kernel = np.zeros(np.shape(membrane_buffer)) * np.nan
+    else:
+        membrane_buffer[:, :-1] = membrane_buffer[:, 1:]
+        membrane_buffer[:, -1] = Vm
+
+    '''Exponential weighing the membrane buffer to reflect more recent
+    fluctuations in Imem. The exponential kernel is choosen to weight
+    the most recent activity with a weight of 1, so we can normalize using
+    the Ispkthr variable.
+    '''
+    exp_weighted_membrane_buffer = np.array(membrane_buffer, copy=True)
+    exp_weighted_membrane_buffer *= kernel[:, :np.shape(exp_weighted_membrane_buffer)[1]]
+
+    activity_proxy = np.sum(exp_weighted_membrane_buffer, axis=1)
+    return activity_proxy
 
 
 @implementation('numpy', discard_units=True)
@@ -167,8 +265,8 @@ def get_activity_proxy_imem(Imem, buffer_pointer, membrane_buffer, kernel):
 
     Args:
         Imem (float): The membrane current of the DPI neuron model.
-        buffer_pointer (TYPE): Description.
-        membrane_buffer (TYPE): Description.
+        buffer_pointer (int): Pointer to keep track of the buffer
+        membrane_buffer (numpy.ndarray): Ring buffer for membrane potential
         decay (int): Time constant of exp. weighted decay
 
     Returns:
@@ -194,20 +292,52 @@ def get_activity_proxy_imem(Imem, buffer_pointer, membrane_buffer, kernel):
     activity_proxy = np.sum(exp_weighted_membrane_buffer, axis=1)
     return activity_proxy
 
-
 @implementation('numpy', discard_units=True)
-@check_units(activity_proxy=amp, old_max=1, result=1)
-def max_value_update(activity_proxy, old_max):
-    if (np.max(activity_proxy / pA) > old_max[0]):
-        old_max[0] = np.array(np.max(activity_proxy / pA), copy=True)
+@check_units(activity_proxy=volt, old_max=1, result=1)
+def max_value_update_vm(activity_proxy, old_max):
+    """ This run_regularly simply calculates the maximum value of the
+    activity proxy to normalize it accordingly.
+
+    Args:
+        activity_proxy(numpy.ndarray): Array containing the exponentially
+            weights membrane potential traces.
+        old_max(float): Maximum value of activity_proxy since the start
+            of the simulation.
+
+    Returns:
+        old_max (float): Updated maximum value
+
+    """
+    if (np.max(activity_proxy / mV) > old_max[0]):
+        old_max[0] = np.array(np.max(activity_proxy / mV), copy=True)
     return old_max[0]
 
 
 @implementation('numpy', discard_units=True)
 @check_units(activity_proxy=amp, old_max=1, result=1)
-def normalize_activity_proxy(activity_proxy, old_max):
-    """This function normalized the variance of Imem, as calculated
-    by get_membrane_variance.
+def max_value_update_imem(activity_proxy, old_max):
+    """ This run_regularly simply calculates the maximum value of the
+    activity proxy to normalize it accordingly.
+
+    Args:
+        activity_proxy(numpy.ndarray): Array containing the exponentially
+            weights membrane potential traces.
+        old_max(float): Maximum value of activity_proxy since the start
+            of the simulation.
+
+    Returns:
+        old_max (float): Updated maximum value
+
+    """if (np.max(activity_proxy / pA) > old_max[0]):
+        old_max[0] = np.array(np.max(activity_proxy / pA), copy=True)
+    return old_max[0]
+
+
+@implementation('numpy', discard_units=True)
+@check_units(activity_proxy=volt, old_max=1, result=1)
+def normalize_activity_proxy_vm(activity_proxy, old_max):
+    """This function normalized the variance of Vm, as calculated
+    by get_activity_proxy_vm.
 
     Example:
         >>> from teili.core.groups import Neurons, Connections
@@ -226,7 +356,47 @@ def normalize_activity_proxy(activity_proxy, old_max):
                                   method='euler',
                                   name='syn_obj')
 
-        >>> neuron_obj.run_regularly('''normalized_membrane_var = normalize_membrane_variance(membrane_variance, Ispkthr)''',
+        >>> neuron_obj.run_regularly('''normalized_activity_proxy=normalize_activity_proxy(activity_proxy, old_max)''',
+                                     dt=5*ms)
+    Args:
+        activity_proxy (float): exponentially weighted Imem fluctuations
+        old_max (float, volt): Value used to normalize the
+            activity proxy variable
+
+    Returns:
+        float: Normalized activity proxy
+    """
+    if old_max == 0.0:
+        normalized_activity_proxy = np.zeros(np.shape(activity_proxy))
+    else:
+        normalized_activity_proxy = (activity_proxy / mV) / old_max
+
+    return normalized_activity_proxy
+
+@implementation('numpy', discard_units=True)
+@check_units(activity_proxy=amp, old_max=1, result=1)
+def normalize_activity_proxy_imem(activity_proxy, old_max):
+    """This function normalized the activity proxy of Imem, as calculated
+    by get_activity_proxy_imem.
+
+    Example:
+        >>> from teili.core.groups import Neurons, Connections
+        >>> from teili.models.builder.neuron_equation_builder import NeuronEquationBuilder
+        >>> from teili.models.builder.synapse_equation_builder import SynapseEquationBuilder
+
+        >>> DPIvar = NeuronEquationBuilder.import_eq('octa/models/equations/DPIvar',
+                                                         num_inputs=2)
+        >>> DPIvdp = SynapseEquationBuilder.import_eq('octa/models/equations/DPIvdp')
+
+        >>> neuron_obj = Neurons(2, equation_builder=DPIvar_obj,
+                                 name="neuron_obj")
+        >>> syn_obj = Connections(neuron_obj,
+                                  neuron_obj,
+                                  equation_builder=DPIvdp,
+                                  method='euler',
+                                  name='syn_obj')
+
+        >>> neuron_obj.run_regularly('''normalized_activity_proxy=normalize_activity_proxy(activity_proxy, old_max)''',
                                      dt=5*ms)
     Args:
         activity_proxy (float): exponentially weighted Imem fluctuations
@@ -257,6 +427,10 @@ def weight_normalization(weights, source_N, target_N,
     Conceptually this function can be understood as stumulatin all synapses
     at the same time and performing a global operation on the weights if the
     total sum of synaptic weights fall out of the boundaries.
+
+    Args:
+
+    Returns:
     """
     data = np.reshape(weights, (source_N, target_N))
     sum_of_weights = np.sum(data, 1) / source_N
@@ -272,6 +446,29 @@ def weight_normalization(weights, source_N, target_N,
 @implementation('numpy', discard_units=True)
 @check_units(weights=1, source_N=1, target_N=1, re_init_threshold=1, lastspike=second, t=second, result=1)
 def get_re_init_index(weights, source_N, target_N, re_init_threshold, lastspike, t):
+    """ This function provides a boolean vector to indicate if the parameters
+    of a given neuron need to be updated. This is required if two or more
+    variables of a neuron needs to be updated according to the same condition
+    as e.g. in structural plasticity, where the weight values trigger the
+    respawning of synapses. To ensure that the synaptic time constants are
+    resampled accordingly a boolen vector is required to indictate the update,
+    as brian2 does not support the return of more than one variable from a
+    run_regularly function.
+
+    Args:
+        weights (numpy.ndarray):
+        source_N (int, required):
+        target_N (in_requiredm):
+        re_init_threshold (float, required):
+        lastspike (float, second, required):
+        t (float, second, required):
+
+    Returns:
+        re_init_index (numpy.ndarray): Boolean vector indicating which post-
+            synaptic neuron will be subject to re-sampling of its free
+            parameters.
+
+    """
     data = np.zeros((source_N, target_N)) * np.nan
     data = np.reshape(weights, (source_N, target_N))
     re_init_index = np.mean(data, 0) < re_init_threshold
@@ -285,8 +482,29 @@ def get_re_init_index(weights, source_N, target_N, re_init_threshold, lastspike,
 
 
 @implementation('numpy', discard_units=True)
-@check_units(Vthr=volt, Vm=volt, EL=volt, Vres=volt, VT=volt, sigma_membrane=volt, not_refractory=1, result=volt)
-def update_threshold(Vthr, Vm, EL, Vres, VT, sigma_membrane, not_refractory):
+@check_units(Vthr=volt, Vm=volt, EL=volt, VT=volt, sigma_membrane=volt, not_refractory=1, result=volt)
+def update_threshold(Vthr, Vm, EL, VT, sigma_membrane, not_refractory):
+    """ A function whoch based on the deviation of the membrane potential
+    from its equilibrium potential adjusts the firing threshold.
+    The idea behind this run_regular function was published by
+    Afshar et al. (2019): Event-based Feature Extraction Using Adaptive
+    Selection Thresholds. https://arxiv.org/pdf/1907.07853.pdf
+
+    Args:
+        Vthr (array, volt): Spike thresholds.
+        Vm (array, volt): Membrane potential
+        VT (float, volt): Trigger voltage for Iexp.
+        sigma_membrane (float, volt): Increment/Decrement of the spike
+            threshold.
+        not_refractory (array, bool): Boolean vector indicating if neuron
+            is currently in its refractory period.
+
+    Returns:
+        data (array, volt): Updated spiking thresholds.
+
+    Important note:
+        DOES NOT WRK FOR DPI NEURON
+    """
     data = np.zeros((len(Vthr))) * np.nan
     data = np.asarray(Vthr / mV)
 

7c259c052f712db6806ddd8e2049b88df1335213
mmilde <mmilde@ini.uzh.ch>
2020-01-08 16:59:29 +0100

    Removes run_regularly folder from setup.py

--- setup.py
+++ setup.py
@@ -138,7 +138,6 @@ setup(
         'teili.tools.visualizer.DataControllers',
         'teili.tools.visualizer.DataModels',
         'teili.tools.visualizer.DataViewers',
-        'teili.tools.run_regularly',
     ],
 
 

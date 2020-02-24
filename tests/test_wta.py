567636a62d0de839d9d1bccc84601c41a7f6ec7b
Alpha <alpren@ini.uzh.ch>
2020-02-24 20:29:17 +0100

    change name of debug flag of building block to verbose in test_wta

--- tests/test_wta.py
+++ tests/test_wta.py
@@ -15,19 +15,19 @@ prefs.codegen.target = "numpy"
 class TestWTA(unittest.TestCase):
 
     def test_attributes_1D(self):
-        test1DWTA = WTA(name='test1DWTA', dimensions=1, num_neurons=16, debug=False)
+        test1DWTA = WTA(name='test1DWTA', dimensions=1, num_neurons=16, verbose=False)
         self.assertEqual(test1DWTA.spike_gen.N, 16)
         self.assertEqual(test1DWTA.num_neurons, 16)
         self.assertEqual(test1DWTA._groups['n_exc'].N, 16)
 
     def test_attributes_2D(self):
-        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, debug=False)
+        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, verbose=False)
         self.assertEqual(test2DWTA.spike_gen.N, 16**2)
         self.assertEqual(test2DWTA.num_neurons, 16)
         self.assertEqual(test2DWTA._groups['n_exc'].N, 16**2)
 
     def test_set_spikes(self):
-        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, debug=False)
+        test2DWTA = WTA(name='test2DWTA', dimensions=2, num_neurons=16, verbose=False)
         neuron_ts = np.arange(0, 51, 5)
         neuron_id = np.ones((len(neuron_ts))) * (16**2 / 2)
         test2DWTA.spike_gen.set_spikes(indices=neuron_id, times=neuron_ts * ms)

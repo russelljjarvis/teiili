ebaa2b457eeedd5df21c54f3e8da114370a3127c
mmilde <mmilde@ini.uzh.ch>
2020-03-11 17:28:21 +1100

    Adds script to install/move tutprials+unit_test to user directory as post-install commands are not supported on pypi.

--- teili/tools/generate_teiliApps.py
+++ teili/tools/generate_teiliApps.py
@@ -6,7 +6,7 @@ from teili.models import neuron_models, synapse_models
 def generate_user_directory():
     path = os.path.expanduser("~")
     current_file_path = os.path.dirname(os.path.abspath(__file__))
-    teili_home = Path(current_file_path).parent
+    teili_home = Path(current_file_path).parent.parent
 
     equation_path = os.path.join(path, "teiliApps", "equations")
 
@@ -19,14 +19,14 @@ def generate_user_directory():
     if not os.path.isdir(target_path):
         Path(target_path).mkdir(parents=True)
 
-    os.system('cp {}* {}'.format(source_path, target_path))
+    os.system('cp -r {}* {}'.format(source_path, target_path))
 
     source_path = os.path.join(teili_home, "tutorials", "")
     target_path = os.path.join(path, "teiliApps", "tutorials", "")
     if not os.path.isdir(target_path):
         Path(target_path).mkdir(parents=True)
 
-    os.system('cp {}* {}'.format(source_path, target_path))
+    os.system('cp -r {}* {}'.format(source_path, target_path))
 
 if __name__ == '__main__':
     generate_user_directory()

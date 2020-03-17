c2739c22cf800f946fef3dc4f1785a810d18a437
mmilde <mmilde@ini.uzh.ch>
2020-03-17 20:56:18 +1100

    WIP Removes tkinter from imports and loading function.

--- teili/tools/sorting.py
+++ teili/tools/sorting.py
@@ -25,7 +25,7 @@ Example:
 # @Date:   2018-06-05 11:09:20
 
 import numpy as np
-from tkinter import filedialog
+# from tkinter import filedialog
 import warnings
 
 
@@ -82,11 +82,14 @@ class SortMatrix():
             ndarray: loaded matrix from file.
         """
 
-        if self.filename is not None:
+        # if self.filename is not None:
+        try:
             matrix = np.load(self.filename)
-        else:
-            self.filename = filedialog.askopenfilename()
-            matrix = np.load(self.filename)
+        except TypeError:
+            print("Please specify a valid filename.")
+            return None
+            # self.filename = filedialog.askopenfilename()
+            # matrix = np.load(self.filename)
 
         self.matrix = matrix.reshape((self.nrows, self.ncols))
         return self.matrix

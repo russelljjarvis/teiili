9742c030ccc78ecd1ace3a81adb1bb8b7d1ab301
pabloabur <pabloabur@gmail.com>
2020-05-28 12:01:49 +1000

    Improve SLIF tutorial
    Changes were made to make the code more structured and clear. Some
    variables were renamed, run_regularly function is now more modular and
    unnecessary variables were removed.

--- tutorials/SLIF_tutorial.py
+++ tutorials/SLIF_tutorial.py
@@ -20,20 +20,22 @@ from teili.models.builder.neuron_equation_builder import NeuronEquationBuilder
 
 defaultclock.dt = 1*ms
 
-# TODO modular functions, n_bits as parameter
 @implementation('numpy', discard_units=True)
-@check_units(lfsr_in=1, num_neurons=1, result=1)
-def LFSR_20bit(lfsr_in, num_neurons):
+@check_units(lfsr_in=1, num_neurons=1, num_bits=1, result=1)
+def LFSR_gen(lfsr_in, num_neurons, num_bits):
     """
-    Generate pseudorandom number with a 20-bit Linear Feedback Shift Register
-    (LFSR) for num_neurons neurons.
+    Generate a pseudorandom number between 0 and 1 with a 20-bit Linear
+    Feedback Shift Register (LFSR). This is equivalent to generating random
+    numbers from an uniform distribution.
 
     This function receives a given number and performs num_neurons iterations
-    of the LFSR. The LFSR does a circular shift (i.e. all the values are
-    shifted left while the previous MSB becomes the new LSB) and ensures the
-    variable is no bigger than 20 bits. After that, the 3rd bit is update with
-    the result of a XOR between bits 3 and 0. Note that, for convenience, the
-    input and outputs are normalized, i.e. value/2**20.
+    of the LFSR. This is done to set the next input that will be used when a
+    given neuron needs another random number. The LFSR does a circular shift
+    (i.e. all the values are shifted left while the previous MSB becomes the
+    new LSB) and ensures the variable is no bigger than 20 bits. After that,
+    the 3rd bit is update with the result of a XOR between bits 3 and 0. Note
+    that, for convenience, the input and outputs are normalized, i.e.
+    value/2**20.
 
     Parameters
     ----------
@@ -41,22 +43,26 @@ def LFSR_20bit(lfsr_in, num_neurons):
         Value between 0 and 1 that will be the input to the LFSR
     num_neurons : int
         Number of neurons in the group
+    num_bits : int
+        Number of bits of the LFSR
 
     Returns
     -------
-    lfsr_out : float
-        Output of the LFSR
+    float
+        A random number between 0 and 1
 
     Examples
     --------
     >>> number = 2**19 + 2**2
     >>> bin(number)
     '0b10000000000000000100'
-    >>> bin(int(LFSR_20bit(number/2**20, 1)2**20))
+    >>> bin(int(LFSR_gen(number/2**20, 1)2**20))
     '0b1'
     """
-    num_bits = 20
-    lfsr_in *= 2**20
+    num_bits = int(num_bits)
+    lfsr_in *= 2**num_bits
+    mask = 2**num_bits - 1
+
     for _ in range(num_neurons):
         lfsr_in = int(lfsr_in) << 1
         overflow = True if lfsr_in & (1 << num_bits) else False
@@ -64,7 +70,6 @@ def LFSR_20bit(lfsr_in, num_neurons):
         if overflow:
             lfsr_in |= 1
         # Ensures variable is num_bits long
-        mask = 2**num_bits - 1
         lfsr_in = lfsr_in & mask
         # Get bits from positions 0 and 3
         fourth_tap = 1 if lfsr_in & (1 << 3) else 0
@@ -74,55 +79,52 @@ def LFSR_20bit(lfsr_in, num_neurons):
         if bool(fourth_tap^first_tap):
             lfsr_in |= (1 << 3)
 
-    lfsr_out = lfsr_in/2**20
+    return lfsr_in/2**num_bits
 
-    return lfsr_out
-
-def init_lfsr(lfsr_seed, num_neurons):
+def init_lfsr(lfsr_seed, num_neurons, num_bits):
     """
     Initializes numbers that will be used for each neuron on the LFSR
     function by iterating on the LFSR num_neurons times.
 
     Parameters
     ----------
-    lfsr_seed : float
-        Value between 0 and 1 that will be the input to the LFSR
+    lfsr_seed : int
+        The seed of the LFSR
     num_neurons : int
         Number of neurons in the group
+    num_bits : int
+        Number of bits of the LFSR
 
     Returns
     -------
-    lfsr_out : float
-        Output of the LFSR
+    lfsr_out : numpy.array of float
+        The initial values of each neuron
 
     """
-    num_bits = 20
     lfsr_out = [0 for _ in range(num_neurons)]
-    # Set initial value from provided seed, as in barebone python code
-    lfsr_tmp = int(lfsr_seed)
+    mask = 2**num_bits - 1
 
     for i in range(num_neurons):
-        lfsr_tmp = lfsr_tmp << 1
-        overflow = True if lfsr_tmp & (1 << num_bits) else False
+        lfsr_seed = lfsr_seed << 1
+        overflow = True if lfsr_seed & (1 << num_bits) else False
 
         # Re-introduces 1s beyond last position
         if overflow:
-            lfsr_tmp |= 1
-        
+            lfsr_seed |= 1
+
         # Ensures variable is num_bits long
-        mask = 2**num_bits - 1
-        lfsr_tmp = lfsr_tmp & mask
+        lfsr_seed = lfsr_seed & mask
 
         # Get bits from positions 0 and 3
-        fourth_tap = 1 if lfsr_tmp & (1 << 3) else 0
-        first_tap = 1 if lfsr_tmp & (1 << 0) else 0
+        fourth_tap = 1 if lfsr_seed & (1 << 3) else 0
+        first_tap = 1 if lfsr_seed & (1 << 0) else 0
         # Update bit in position 3
-        lfsr_tmp &=~ (1 << 3)
+        lfsr_seed &=~ (1 << 3)
         if bool(fourth_tap^first_tap):
-            lfsr_tmp |= (1 << 3)
-        lfsr_out[i] = lfsr_tmp
+            lfsr_seed |= (1 << 3)
+        lfsr_out[i] = lfsr_seed
 
-    return np.asarray(lfsr_out)/2**20
+    return np.asarray(lfsr_out)/2**num_bits
 
 N = 4
 duration = 400*ms
@@ -139,11 +141,15 @@ stochastic_decay = ExplicitStateUpdater('''x_new = dt*f(x,t)''')
 neuron = Neurons(N, equation_builder=builder_object, method=stochastic_decay,
                  name="test_neurons", verbose=True)
 
-neuron.Vmem = 3
 lfsr_seed = 12345
-neuron.lfsr_input = init_lfsr(lfsr_seed, neuron.N)
-neuron.namespace.update({'LFSR_20bit': LFSR_20bit})
-neuron.run_regularly('''lfsr_input = LFSR_20bit(lfsr_input, N)
+lfsr_len = 20
+neuron.add_state_variable('lfsr_len', shared=True, constant=True)
+neuron.lfsr_len = lfsr_len
+neuron.rand_input = init_lfsr(lfsr_seed, neuron.N, lfsr_len)
+neuron.namespace.update({'LFSR_gen': LFSR_gen})
+
+neuron.Vmem = 3
+neuron.run_regularly('''rand_input = LFSR_gen(rand_input, N, lfsr_len)
                         ''',
                      dt=1*ms)
 
@@ -158,7 +164,6 @@ Net.run(duration)
 #ISI = spike_times[1:-1] - spike_times[0:-2]
 #_ = plt.hist(ISI, bins=20, range=(0, 100))
 
-#import pdb; pdb.set_trace()
 plt.figure()
 plt.plot(spikemon.t/ms, spikemon.i, 'ro')
 plt.ylabel('Neuron index')

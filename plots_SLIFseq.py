500c91b38dbadafb4e7f8e046fcb1a1292ec4e5c
pabloabur <pabloabur@ini.uzh.ch>
2020-10-15 16:06:31 +1100

    Tests network without recurrent connections

--- plots_SLIFseq.py
+++ plots_SLIFseq.py
@@ -1,5 +1,6 @@
 import numpy as np
 import copy
+import json
 
 from pyqtgraph.Qt import QtGui
 import pyqtgraph as pg
@@ -9,60 +10,69 @@ from pathlib import Path
 
 from teili.tools.sorting import SortMatrix
 
-trials = []
-for i in Path('/home/pablo/data_low_number_neurons/').glob('data_3_50_0.85_2_2_-3.*'):
-    trials.append(i)
+data_files = [] # TODO now each is a datafile
+data_folder = './2020.10.15_15.40/'
+for i in Path(data_folder).glob('*.npz'):
+    data_files.append(i)
 
 app = QtGui.QApplication([])
 win = QtGui.QMainWindow()
 area = DockArea()
 win.setCentralWidget(area)
 
-docks = []
-for trial in trials:
-    docks.append(Dock(trial.name[4:-4], size=(1, 1)))
-for dock in docks:
-    area.addDock(dock, 'left')
-for i in range(len(docks) - 1):
-    area.moveDock(docks[i-1], 'above', docks[i-2])
+trials = ['maoi'] # TODO this should now be whatever I want on docks
+d1 = Dock('traces and raster', size=(1, 1))
+d2 = Dock('matrices', size=(1, 1))
+d3 = Dock('receptive fields', size=(1, 1))
+area.addDock(d1, 'left')
+area.addDock(d2, 'left')
+area.addDock(d3, 'left')
+area.moveDock(d2, 'above', d3)
+area.moveDock(d1, 'above', d2)
 
-for i, trial in enumerate(trials):
-    num_inh = trial.name[:-4].split('_')[1]
-    input_rate = trial.name[:-4].split('_')[2]
-    ei_conn = trial.name[:-4].split('_')[3]
-    ffi_weight = trial.name[:-4].split('_')[4]
-    ei_weight = trial.name[:-4].split('_')[5]
-    ie_weight = trial.name[:-4].split('_')[6]
-    num_exc = 15
-    num_channels = 30
-    data=np.load(trial, allow_pickle=True)
+# Load metadata of given simulation
+with open(f'{data_folder}metadata.json', 'r') as f:
+    metadata = json.load(f)
+num_inh = metadata['num_inh']
+input_rate = metadata['input_rate']
+ei_conn = metadata['e->i p']
+#ffi_weight = metadata['']
+num_exc = metadata['num_exc']
+num_channels = metadata['num_channels']
+rasters = np.load(f'{data_folder}rasters.npz')
+traces = np.load(f'{data_folder}traces.npz')
+matrices = np.load(f'{data_folder}matrices.npz', allow_pickle=True)
+plot_d1, plot_d2, plot_d3 = True, True, True
 
-    # Avoid storing too much data on memory
-    data_length = -10000
-    input_t = data['input_t'][data_length:]
-    input_i = data['input_i'][data_length:]
-    neuron_id_e = np.random.randint(0, num_exc)
-    neuron_id_i = np.random.randint(0, num_inh)
-    Vm_e = data['Vm_e'][neuron_id_e][data_length:]
-    Vm_i = data['Vm_i'][neuron_id_i][data_length:]
-    exc_spikes_t = data['exc_spikes_t'][data_length:]
-    exc_spikes_i = data['exc_spikes_i'][data_length:]
-    inh_spikes_t = data['inh_spikes_t'][data_length:]
-    inh_spikes_i = data['inh_spikes_i'][data_length:]
-    exc_rate_t = data['exc_rate_t'][data_length:]
-    exc_rate = data['exc_rate'][data_length:]
-    inh_rate_t = data['inh_rate_t'][data_length:]
-    inh_rate = data['inh_rate'][data_length:]
-    rf = data['rf']
-    am = data['am'][:, data_length:]
-    rec_ids = data['rec_ids']
-    rec_w = data['rec_w']
-    del data
+# Avoid storing too much data on memory
+data_start, data_end = 0, -1
+input_t = rasters['input_t'][data_start:data_end]
+input_i = rasters['input_i'][data_start:data_end]
+neuron_id_e = np.random.randint(0, num_exc)
+neuron_id_i = np.random.randint(0, num_inh)
+Vm_e = traces['Vm_e'][neuron_id_e][data_start:data_end]
+Vm_i = traces['Vm_i'][neuron_id_i][data_start:data_end]
+exc_spikes_t = rasters['exc_spikes_t'][data_start:data_end]
+exc_spikes_i = rasters['exc_spikes_i'][data_start:data_end]
+inh_spikes_t = rasters['inh_spikes_t'][data_start:data_end]
+inh_spikes_i = rasters['inh_spikes_i'][data_start:data_end]
+exc_rate_t = traces['exc_rate_t'][data_start:data_end]
+exc_rate = traces['exc_rate'][data_start:data_end]
+inh_rate_t = traces['inh_rate_t'][data_start:data_end]
+inh_rate = traces['inh_rate'][data_start:data_end]
+rf = matrices['rf']
+#am = matrices['am'][:, data_start:data_end]
+#rec_ids = matrices['rec_ids'] FIXME
+#rec_w = matrices['rec_w']
+del matrices
+del rasters
+del traces
 
+if plot_d1:
     l = pg.LayoutWidget()
-    text = f"""Num. inh. neuron: {num_inh}, Input Poisson rate: {input_rate}, e->i conn.: {ei_conn} feedforward inh. w.: {ffi_weight}, e->i w.: {ei_weight}, i->e w.: {ie_weight}"""
+    text = f"""{metadata}"""
     l.addLabel(text)
-    docks[i].addWidget(l, 0, 0, colspan=2)
+    d1.addWidget(l, 0, 0, colspan=2)
 
     p1 = pg.PlotWidget(title='Input')
     p1.plot(input_t*1e-3, input_i, pen=None, symbolSize=3, symbol='o')
@@ -74,8 +84,8 @@ for i, trial in enumerate(trials):
     p2.setYRange(0, 0.025)
     p2.setLabel('left', 'Membrane potential', units='V')
     p2.setLabel('bottom', 'Time', units='s')
-    docks[i].addWidget(p1, 1, 0)
-    docks[i].addWidget(p2, 1, 1)
+    d1.addWidget(p1, 1, 0)
+    d1.addWidget(p2, 1, 1)
 
     p3 = pg.PlotWidget(title='Raster plot (exc. pop.)')
     p3.plot(exc_spikes_t*1e-3, exc_spikes_i, pen=None, symbolSize=3,
@@ -89,8 +99,8 @@ for i, trial in enumerate(trials):
     p4.setLabel('left', 'Neuron index')
     p4.setLabel('bottom', 'Time', units='s')
     p4.setXLink(p1)
-    docks[i].addWidget(p3, 2, 0)
-    docks[i].addWidget(p4, 2, 1)
+    d1.addWidget(p3, 2, 0)
+    d1.addWidget(p4, 2, 1)
 
     p5 = pg.PlotWidget(title='Population rate (exc.)')
     p5.plot(exc_rate_t*1e-3,
@@ -106,21 +116,22 @@ for i, trial in enumerate(trials):
     p6.setLabel('bottom', 'Time', units='s')
     p6.setLabel('left', 'Rate', units='Hz')
     p6.setXLink(p1)
-    docks[i].addWidget(p5, 3, 0)
-    docks[i].addWidget(p6, 3, 1)
+    d1.addWidget(p5, 3, 0)
+    d1.addWidget(p6, 3, 1)
 
-    # Plot matrices
-    colors = [
-        (0, 0, 0),
-        (0, 0, 255),
-        (255, 255, 0),
-        (255, 255, 255)
-    ]
-    cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 4), color=colors)
-    sorted_w = SortMatrix(ncols=num_exc, nrows=num_exc, matrix=rec_w,
-            fill_ids=rec_ids) #TODO change axis
-    sorted_i = np.asarray([np.where(
-                    np.asarray(sorted_w.permutation) == int(i))[0][0] for i in exc_spikes_i])
+# Plot matrices
+colors = [
+    (0, 0, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (255, 255, 255)
+]
+cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 4), color=colors)
+if plot_d2:
+    #sorted_w = SortMatrix(ncols=num_exc, nrows=num_exc, matrix=rec_w, FIXME
+    #        fill_ids=rec_ids) #TODO change axis
+    #sorted_i = np.asarray([np.where(
+    #                np.asarray(sorted_w.permutation) == int(i))[0][0] for i in exc_spikes_i])
 
     image_axis = pg.PlotItem()
     image_axis.setLabel(axis='bottom', text='RF pixels')
@@ -147,12 +158,44 @@ for i, trial in enumerate(trials):
     m3.ui.histogram.hide()
     m3.ui.roiBtn.hide()
     m3.ui.menuBtn.hide() 
-    m3.setImage(np.reshape(rf, (num_channels, num_exc, -1))[:, sorted_w.permutation,-1], axes={'y':0, 'x':1})
+    m3.setImage(np.reshape(rf, (num_channels, num_exc, -1))[:,:,-1], axes={'y':0, 'x':1})# [:, sorted_w.permutation,-1] FIXME
     m3.setColorMap(cmap)
-    docks[i].addWidget(m1, 1, 3)
-    docks[i].addWidget(m2, 2, 3)
-    docks[i].addWidget(m3, 3, 3)
+    m4 = pg.PlotWidget(title='Population rate')
+    m4.plot(exc_rate_t*1e-3,
+            exc_rate,
+            pen='r')
+    m4.plot(inh_rate_t*1e-3,
+            inh_rate,
+            pen='b')
+    m4.setLabel('bottom', 'Time', units='s')
+    m4.setLabel('left', 'Rate', units='Hz')
+    d2.addWidget(m1, 0, 0)
+    d2.addWidget(m2, 0, 1)
+    d2.addWidget(m3, 0, 2)
+    d2.addWidget(m4, 1, colspan=3)
+
+# Plot receptive fields for each neuron
+if plot_d3:
+    last_frame = np.reshape(rf, (num_channels, num_exc, -1))[:,:,-1]
+    dims = np.sqrt(num_channels).astype(int)
+    rfs = []
+    j = 0
+    k = 0
+    for i in range(num_exc):
+        rfs.append(pg.ImageView())
+        rfs[-1].ui.histogram.hide()
+        rfs[-1].ui.roiBtn.hide()
+        rfs[-1].ui.menuBtn.hide() 
+        rfs[-1].setImage(np.reshape(last_frame[:, i], (dims, dims)), axes={'y':0, 'x':1})# [:, sorted_w.permutation,-1] FIXME
+        rfs[-1].setColorMap(cmap)
+        d3.addWidget(rfs[-1], j, k)
+        if j < np.sqrt(num_exc)-1:
+            j += 1
+        else:
+            j = 0
+            k += 1
 win.show()
+QtGui.QApplication.instance().exec_()
 # TODO those are global and for all t, should adapt
 #spikemon.count/duration) and/or
 # _ = hist(spikemon.t/ms, 100, histtype='stepfilled', facecolor='k', weights=list(ones(len(spikemon))/(N*defaultclock.dt)))
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -2,105 +2,149 @@
 import numpy as np
 from teili.tools.sorting import SortMatrix
 import random
+import copy
 
 import matplotlib.pyplot as plt
 
 # With required diag>=1, only 8x8 start to look like a fair test
-trials = {#'trial1': [True, 3, 3, 0, False, .5, .7],
-          'trial4': [False, 8, 8, 1, False, .5, .7],
-          'trial5': [True, 8, 8, 1, False, .5, .7],
-          'trial2': [False, 20, 20, 1, False, .5, .7],
-          'trial3': [True, 20, 20, 1, False, .5, .7],
-          }
+#trials = {#'trial1': [True, 3, 3, 0, False, .5, .7],
+#          'trial4': [False, 8, 8, 1, False, .5, .7],
+#          'trial5': [True, 8, 8, 1, False, .5, .7],
+#          'trial2': [False, 20, 20, 1, False, .5, .7],
+#          'trial3': [True, 20, 20, 1, False, .5, .7],
+#          }
+#
+#for values in trials.values():
+#    # Base matrix
+#    noise = values[0]
+#    n_rows = values[1] 
+#    n_cols = values[2] 
+#    diag_width = values[3]
+#    px_grad = values[4]
+#    min_val = values[5]
+#    max_val = values[6]
+#    if n_rows < (2*diag_width+1):
+#        print('Matrix too small for data')
+#        import sys;sys.exit()
+#
+#    # First 3 plots of each trial:
+#    # 3 diagonals matrix
+#    test_matrix = np.zeros((n_rows, n_cols))
+#    aux_vec = np.zeros(n_cols)
+#    aux_vec[0] = 1
+#    aux_vec[int(n_cols/2)] = 1
+#    for i in range(n_rows):
+#        # Define where to add values
+#        central_ids = np.where(aux_vec==1)[0]
+#        add_to_ids = np.append(
+#            np.arange(central_ids[0]-diag_width, central_ids[0]+diag_width+1),
+#            np.arange(central_ids[1]-diag_width, central_ids[1]+diag_width+1)
+#            )
+#        # Create gradient around central pixels
+#        temp_values = np.linspace(min_val, max_val, num=diag_width+1)
+#        if not px_grad:
+#            temp_values[:] = max_val
+#        values_to_add = np.concatenate([temp_values, temp_values[::-1][1:],
+#                                        temp_values, temp_values[::-1][1:]])
+#        # Remove values outside dimensions of matrix
+#        values_to_add = np.delete(values_to_add, np.append(
+#            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
+#        add_to_ids = np.delete(add_to_ids, np.append(
+#            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
+#        test_matrix[i][add_to_ids]=values_to_add
+#        if noise:
+#            test_matrix[i][np.random.randint(n_cols, size=int(n_cols/2) - 1)] = 0
+#        aux_vec = np.roll(aux_vec, 1)
+#
+#    sm3 = SortMatrix(ncols=n_cols, nrows=n_rows, matrix=test_matrix)
+#    plt.figure()
+#    plt.imshow(sm3.matrix)
+#    plt.title('original')
+#    plt.figure()
+#    plt.imshow(sm3.matrix[sm3.permutation, :])
+#    plt.title('sorted with permutation indices')
+#    plt.figure()
+#    plt.imshow(sm3.sorted_matrix)
+#    plt.title('sorted_matrix attribute')
+#    plt.show()
+#
+#    # Last 4 plots of each trial:
+#    # 1 diagonal matrix
+#    test_matrix=np.zeros((n_rows, n_cols))
+#    ids = [x for x in range(n_rows)]
+#    for central_id in ids:
+#        # Define where to add values
+#        add_to_ids = np.arange(central_id-diag_width, central_id+diag_width+1)
+#        # Create gradient around central pixel
+#        temp_values = np.linspace(min_val, max_val, num=diag_width+1)
+#        if not px_grad:
+#            temp_values[:] = max_val
+#        values_to_add = np.append(temp_values, temp_values[::-1][1:])
+#        # Remove values outside dimensions of matrix
+#        values_to_add = np.delete(values_to_add, np.append(
+#            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
+#        add_to_ids = np.delete(add_to_ids, np.append(
+#            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
+#        test_matrix[central_id][add_to_ids]=values_to_add
+#        if noise:
+#            test_matrix[central_id][np.random.randint(n_cols, size=int(n_cols/2) - 1)] = 0
+#
+#    plt.figure()
+#    plt.imshow(test_matrix)
+#    plt.title('original')
+#    random.shuffle(ids)
+#    test_matrix = test_matrix[ids]
+#    sm1 = SortMatrix(ncols=n_cols, nrows=n_rows, matrix=test_matrix)
+#    plt.figure()
+#    plt.imshow(sm1.matrix)
+#    plt.title('original shuffled')
+#    plt.figure()
+#    plt.imshow(sm1.matrix[sm1.permutation, :])
+#    plt.title('sorted with permutation indices')
+#    plt.figure()
+#    plt.imshow(sm1.sorted_matrix)
+#    plt.title('sorted_matrix attribute')
+#    plt.show()
 
-for values in trials.values():
-    # Base matrix
-    noise = values[0]
-    n_rows = values[1] 
-    n_cols = values[2] 
-    diag_width = values[3]
-    px_grad = values[4]
-    min_val = values[5]
-    max_val = values[6]
-    if n_rows < (2*diag_width+1):
-        print('Matrix too small for data')
-        import sys;sys.exit()
+# from moritz
+test_matrix = np.zeros((4,4)) 
+test_matrix = np.random.rand(4,4)
+test_matrix = np.clip(test_matrix, 0, 0.75)
+shuffled_matrix = np.zeros((4,4)) * np.nan
 
-    # First 3 plots of each trial:
-    # 3 diagonals matrix
-    test_matrix = np.zeros((n_rows, n_cols))
-    aux_vec = np.zeros(n_cols)
-    aux_vec[0] = 1
-    aux_vec[int(n_cols/2)] = 1
-    for i in range(n_rows):
-        # Define where to add values
-        central_ids = np.where(aux_vec==1)[0]
-        add_to_ids = np.append(
-            np.arange(central_ids[0]-diag_width, central_ids[0]+diag_width+1),
-            np.arange(central_ids[1]-diag_width, central_ids[1]+diag_width+1)
-            )
-        # Create gradient around central pixels
-        temp_values = np.linspace(min_val, max_val, num=diag_width+1)
-        if not px_grad:
-            temp_values[:] = max_val
-        values_to_add = np.concatenate([temp_values, temp_values[::-1][1:],
-                                        temp_values, temp_values[::-1][1:]])
-        # Remove values outside dimensions of matrix
-        values_to_add = np.delete(values_to_add, np.append(
-            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
-        add_to_ids = np.delete(add_to_ids, np.append(
-            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
-        test_matrix[i][add_to_ids]=values_to_add
-        if noise:
-            test_matrix[i][np.random.randint(n_cols, size=int(n_cols/2) - 1)] = 0
-        aux_vec = np.roll(aux_vec, 1)
+test_matrix[0,0] = 1.
+test_matrix[1,1] = 1.
+test_matrix[2,2] = 1.
+test_matrix[3,3] = 1.
+test_matrix[0,1] = 1.
+test_matrix[1,0] = 1.
+test_matrix[1,2] = 1.
+test_matrix[2,1] = 1.
+test_matrix[2,3] = 1.
+test_matrix[3,2] = 1.
 
-    sm3 = SortMatrix(ncols=n_cols, nrows=n_rows, matrix=test_matrix)
-    plt.figure()
-    plt.imshow(sm3.matrix)
-    plt.title('original')
-    plt.figure()
-    plt.imshow(sm3.matrix[sm3.permutation, :])
-    plt.title('sorted with permutation indices')
-    plt.figure()
-    plt.imshow(sm3.sorted_matrix)
-    plt.title('sorted_matrix attribute')
-    plt.show()
+rnd_ind = np.asarray([3,1,0,2])
+shuffled_matrix[:, rnd_ind] = test_matrix
 
-    # Last 4 plots of each trial:
-    # 1 diagonal matrix
-    test_matrix=np.zeros((n_rows, n_cols))
-    ids = [x for x in range(n_rows)]
-    for central_id in ids:
-        # Define where to add values
-        add_to_ids = np.arange(central_id-diag_width, central_id+diag_width+1)
-        # Create gradient around central pixel
-        temp_values = np.linspace(min_val, max_val, num=diag_width+1)
-        if not px_grad:
-            temp_values[:] = max_val
-        values_to_add = np.append(temp_values, temp_values[::-1][1:])
-        # Remove values outside dimensions of matrix
-        values_to_add = np.delete(values_to_add, np.append(
-            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
-        add_to_ids = np.delete(add_to_ids, np.append(
-            np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
-        test_matrix[central_id][add_to_ids]=values_to_add
-        if noise:
-            test_matrix[central_id][np.random.randint(n_cols, size=int(n_cols/2) - 1)] = 0
+s = SortMatrix(nrows=4, ncols=4, axis=1, matrix=copy.deepcopy(shuffled_matrix))
 
-    plt.figure()
-    plt.imshow(test_matrix)
-    plt.title('original')
-    random.shuffle(ids)
-    test_matrix = test_matrix[ids]
-    sm1 = SortMatrix(ncols=n_cols, nrows=n_rows, matrix=test_matrix)
-    plt.figure()
-    plt.imshow(sm1.matrix)
-    plt.title('original shuffled')
-    plt.figure()
-    plt.imshow(sm1.matrix[sm1.permutation, :])
-    plt.title('sorted with permutation indices')
-    plt.figure()
-    plt.imshow(sm1.sorted_matrix)
-    plt.title('sorted_matrix attribute')
-    plt.show()
+plt.figure()
+plt.title('Constructed matrix (by hand)')
+plt.imshow(test_matrix)
+plt.colorbar()
+
+plt.figure()
+plt.title('Unsorted matrix')
+plt.imshow(shuffled_matrix)
+plt.colorbar()
+
+plt.figure()
+plt.title('Sorted matrix')
+plt.imshow(s.sorted_matrix, vmin=0, vmax=1)
+plt.colorbar()
+
+plt.figure()
+plt.title('Permuted matrix')
+plt.imshow(shuffled_matrix[:, s.permutation], vmin=0, vmax=1)
+plt.colorbar()
+plt.show()

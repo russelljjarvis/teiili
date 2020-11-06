a1cdf41601109c985914bfb5ffbfcead8a57589f
Moritz B. Milde <m.milde@westernsydney.edu.au>
2020-11-06 18:32:27 +0100

    Adds functionality to add noise to arbitrary size matrices
    Added two parameters noise (boolean) and noise_probability (float) to
    dynamically add noise (small values) to the matrix. In that way we can
    test when the sorting function, i.e. the similiarity measure breaks
    down. We should think about alternative similarity estimation which is
    not based on eucledian distance (L2 norm).

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -10,31 +10,40 @@ import matplotlib.pyplot as plt
 n_rows = 50
 n_cols = 50
 diag_width = 3
+max_weight = 15
+noise = True 
+noise_probability = 0.05
 
 test_matrix = np.zeros((n_rows, n_cols))
-test_matrix = (13*np.random.rand(n_rows, n_cols)).astype(int)
-test_matrix = np.clip(test_matrix, 0, 15)
+if max_weight > 1:
+    test_matrix = (max_weight*np.random.rand(n_rows, n_cols)).astype(int)
+    test_matrix = np.clip(test_matrix, 0, int(max_weight/2))
+else:
+    test_matrix = (max_weight*np.random.rand(n_rows, n_cols))
+    test_matrix = np.clip(test_matrix, 0, max_weight)
+    
 
 # Construct matrix
 ids = [x for x in range(n_rows)]
 for central_id in ids:
     # Add values
     add_to_ids = np.arange(central_id-diag_width, central_id+diag_width+1)
-    values_to_add = np.array([15 for _ in range(2*diag_width+1)])
+    values_to_add = np.array([max_weight for _ in range(2*diag_width+1)])
     # Remove values outside dimensions of matrix
     values_to_add = np.delete(values_to_add, np.append(
         np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
     add_to_ids = np.delete(add_to_ids, np.append(
         np.where(add_to_ids<0), np.where(add_to_ids>n_rows-1)))
-    test_matrix[central_id][add_to_ids] = values_to_add
+    try:
+        test_matrix[central_id][add_to_ids] = values_to_add
+    except IndexError:
+        continue
 
 # Manually add noise
-test_matrix[0,1] = 0.
-test_matrix[1,1] = 0.
-test_matrix[3,3] = 0.
-test_matrix[4,4] = 0.
-test_matrix[5,4] = 0.
-test_matrix[7,6] = 0.
+if noise:
+    noise_ind = np.where(np.random.rand(n_rows, n_cols) < noise_probability)
+    test_matrix[noise_ind] = 0.3
+
 
 # Shuffle matrix
 shuffled_matrix = np.zeros((n_rows, n_cols))
@@ -43,18 +52,26 @@ np.random.shuffle(inds)
 shuffled_matrix[:, inds] = test_matrix
 sm1 = SortMatrix(ncols=n_cols, nrows=n_rows, axis=1, matrix=copy.deepcopy(shuffled_matrix))
 
-plt.figure()
+
+plt.figure(figsize=(11,8))
+plt.subplot(2,2,1)
 plt.imshow(test_matrix)
 plt.title('Original matrix')
 plt.colorbar()
 
-plt.figure()
+plt.subplot(2,2,2)
 plt.imshow(shuffled_matrix)
 plt.title('Shuffled matrix')
 plt.colorbar()
 
-plt.figure()
+plt.subplot(2,2,3)
 plt.imshow(shuffled_matrix[:, sm1.permutation])
 plt.title('Sorted with permutation indices')
 plt.colorbar()
+
+plt.subplot(2,2,4)
+plt.imshow(sm1.sorted_matrix)
+plt.title('Sorted matrix')
+plt.colorbar()
+
 plt.show()

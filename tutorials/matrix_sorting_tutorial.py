5795bea9888243d4437e2dec58ed94fd828ec07c
pabloabur <pabloabur@gmail.com>
2020-11-25 15:30:30 +1100

    Adds plots for sorting tutorial

--- tutorials/matrix_sorting_tutorial.py
+++ tutorials/matrix_sorting_tutorial.py
@@ -1,10 +1,11 @@
-# TODO put this on test_tools.py
 import numpy as np
 from teili.tools.sorting import SortMatrix
 import random
 import copy
 
 import matplotlib.pyplot as plt
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui
 
 # Definitions
 n_rows = 50
@@ -66,29 +67,60 @@ for ind_source, ind_target in enumerate(conn_matrix):
     rec_matrix[ind_source] = shuffled_matrix[ind_source, ind_target]
 rec_matrix = np.array(rec_matrix, dtype=object)
 
-sorted_matrix = SortMatrix(ncols=n_cols, nrows=n_rows, axis=1,
+sorted_matrix1 = SortMatrix(ncols=n_cols, nrows=n_rows, axis=1,
         matrix=copy.deepcopy(rec_matrix), rec_matrix=True,
         fill_ids=conn_matrix)
+sorted_matrix2 = SortMatrix(ncols=n_cols, nrows=n_rows, axis=1,
+        matrix=copy.deepcopy(shuffled_matrix), rec_matrix=True)
 
-# TODO pyqtgraph
-plt.figure(figsize=(11,8))
-plt.subplot(2,2,1)
-plt.imshow(test_matrix)
-plt.title('Original matrix')
-plt.colorbar()
+app = QtGui.QApplication.instance()
+if app is None:
+    app = QtGui.QApplication(sys.argv)
+else:
+    print('QApplication instance already exists: %s' % str(app))
 
-plt.subplot(2,2,2)
-plt.imshow(shuffled_matrix)
-plt.title('Shuffled matrix')
-plt.colorbar()
+app = pg.mkQApp()
+win = QtGui.QMainWindow()
+cw = QtGui.QWidget()
+win.setCentralWidget(cw)
+layout = QtGui.QGridLayout()
+cw.setLayout(layout)
 
-plt.subplot(2,2,3)
-plt.imshow(shuffled_matrix[:, sorted_matrix.permutation])
-plt.title('Sorted with permutation indices')
-plt.colorbar()
+colors = [
+    (0, 0, 0),
+    (0, 0, 255),
+    (0, 255, 0),
+    (255, 255, 0)
+]
+cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 4), color=colors)
 
-plt.subplot(2,2,4)
-plt.imshow(sorted_matrix.sorted_matrix)
-plt.title('Sorted matrix')
-plt.colorbar()
-plt.show()
+image_axis = pg.PlotItem()
+image_axis.setLabel(axis='bottom', text='Original matrix')
+imv = pg.ImageView(view=image_axis)
+imv.setImage(test_matrix)
+imv.setColorMap(cmap)
+layout.addWidget(imv,0,0, 1, 1)
+
+image_axis = pg.PlotItem()
+image_axis.setLabel(axis='bottom', text='Shuffled matrix')
+imv = pg.ImageView(view=image_axis)
+imv.setImage(shuffled_matrix)
+imv.setColorMap(cmap)
+layout.addWidget(imv,0,1, 1, 1)
+
+image_axis = pg.PlotItem()
+image_axis.setLabel(axis='bottom', text='Sorted matrix (conn less than 1)')
+imv = pg.ImageView(view=image_axis)
+imv.setImage(sorted_matrix1.sorted_matrix)
+imv.setColorMap(cmap)
+layout.addWidget(imv,1,0, 1, 1)
+
+image_axis = pg.PlotItem()
+image_axis.setLabel(axis='bottom', text='Sorted matrix (conn equals 1)')
+imv = pg.ImageView(view=image_axis)
+imv.setImage(sorted_matrix2.sorted_matrix)
+imv.setColorMap(cmap)
+layout.addWidget(imv,1,1, 1, 1)
+
+win.show()
+app.exec_()

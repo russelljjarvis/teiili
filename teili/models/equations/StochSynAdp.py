67cc832ea228926d93fd4262a56a0e78d3f30fce
pabloabur <pabloabur@gmail.com>
2020-12-08 19:27:42 +1100

    updates adp equations according to new lfsr

--- teili/models/equations/StochSynAdp.py
+++ teili/models/equations/StochSynAdp.py
@@ -2,13 +2,16 @@ from brian2.units import *
 StochSynAdp = {'model':
 '''
         dI_syn/dt = int(I_syn*decay_syn/mA + decay_probability_syn)*mA/second : amp (clock-driven)
+        decay_probability_syn = lfsr_timedarray( ((seed_syn+t) % lfsr_max_value_syn) + lfsr_init_syn ) / (2**lfsr_num_bits_syn): 1
         Iin{input_number}_post = I_syn * sign(weight)                           : amp (summed)
 
         decay_syn = tau_syn/(tau_syn + dt) : 1
 
         weight                : 1
         w_plast               : 1
-        decay_probability_syn : 1
+        lfsr_max_value_syn : second
+        seed_syn : second
+        lfsr_init_syn : second
         gain_syn              : amp
         tau_syn               : second (constant)
         lfsr_num_bits_syn : 1 # Number of bits in the LFSR used

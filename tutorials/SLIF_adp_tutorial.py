7e30dc75212d7f47b8b3ecf8940ef869144bf18e
pabloabur <pabloabur@gmail.com>
2021-02-05 17:09:02 +1100

    Adds more meaningful plots and parameters in ADP tutorial

--- tutorials/SLIF_adp_tutorial.py
+++ tutorials/SLIF_adp_tutorial.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.stats import norm
 
 from brian2 import Hz, ms, mV, prefs, SpikeMonitor, StateMonitor, defaultclock,\
-    ExplicitStateUpdater, SpikeGeneratorGroup, TimedArray, PoissonGroup
+    ExplicitStateUpdater, SpikeGeneratorGroup, TimedArray, PoissonGroup,\
+    PopulationRateMonitor
 
 from teili.core.groups import Neurons, Connections
 from teili import TeiliNetwork
@@ -36,7 +37,7 @@ stochastic_decay = ExplicitStateUpdater('''x_new = f(x,t)''')
 net = TeiliNetwork()
 
 # Initialize input sequence
-poisson_spikes = PoissonGroup(100, rates=30*Hz)
+poisson_spikes = PoissonGroup(100, rates=50*Hz)
 
 #################
 # Building network
@@ -49,7 +50,7 @@ exc_cells = Neurons(num_exc,
                     name='exc_cells',
                     verbose=True)
 inh_cells = Neurons(num_inh,
-                    equation_builder=neuron_model(num_inputs=2),
+                    equation_builder=neuron_model(num_inputs=3),
                     method=stochastic_decay,
                     name='inh_cells',
                     verbose=True)
@@ -66,6 +67,10 @@ feedforward_exc = Connections(poisson_spikes, exc_cells,
                               equation_builder=static_synapse_model(),
                               method=stochastic_decay,
                               name='feedforward_exc')
+feedforward_inh = Connections(poisson_spikes, inh_cells,
+                              equation_builder=static_synapse_model(),
+                              method=stochastic_decay,
+                              name='feedforward_inh')
 exc_inh_conn = Connections(exc_cells, inh_cells,
                            equation_builder=static_synapse_model(),
                            method=stochastic_decay,
@@ -83,6 +88,7 @@ inh_inh_conn = Connections(inh_cells, inh_cells,
                            method=stochastic_decay,
                            name='inh_inh_conn')
 feedforward_exc.connect()
+feedforward_inh.connect()
 exc_inh_conn.connect()
 inh_exc_conn.connect()
 exc_exc_conn.connect()
@@ -90,6 +96,7 @@ inh_inh_conn.connect(p=.1)
 
 # Time constants
 feedforward_exc.tau_syn = 5*ms
+feedforward_inh.tau_syn = 5*ms
 inh_exc_conn.tau_syn = 10*ms
 exc_inh_conn.tau_syn = 5*ms
 exc_exc_conn.tau_syn = 5*ms
@@ -99,6 +106,7 @@ inh_cells.tau = 10*ms
 
 # LFSR lengths
 feedforward_exc.lfsr_num_bits_syn = 5
+feedforward_inh.lfsr_num_bits_syn = 5
 exc_cells.lfsr_num_bits = 5
 inh_cells.lfsr_num_bits = 5
 inh_exc_conn.lfsr_num_bits_syn = 5
@@ -109,27 +117,29 @@ inh_inh_conn.lfsr_num_bits_syn = 5
 seed = 12
 exc_cells.Vm = 3*mV
 inh_cells.Vm = 3*mV
+inh_exc_conn.inh_learning_rate = 0.01
 
 inh_exc_conn.weight = -1
 inh_exc_conn.w_plast = 1
 inh_inh_conn.weight = -1
 feedforward_exc.weight = 4
-exc_inh_conn.weight = 4
+feedforward_inh.weight = 4
+exc_inh_conn.weight = 1
 exc_exc_conn.weight = 1
 
 # Set LFSRs for each group
 ta = create_lfsr([exc_cells, inh_cells],
                  [inh_exc_conn, feedforward_exc,
                      exc_inh_conn, exc_exc_conn,
-                     inh_inh_conn],
+                     inh_inh_conn, feedforward_inh],
                  defaultclock.dt)
 
 # Add proxy activity group
 activity_proxy_group = [exc_cells]
 add_group_activity_proxy(activity_proxy_group,
-                         buffer_size=400,
+                         buffer_size=300,
                          decay=150)
-variance_th = .5
+variance_th = .9
 inh_exc_conn.variance_th = np.random.uniform(
         low=variance_th - 0.1,
         high=variance_th + 0.1,
@@ -150,23 +160,56 @@ statemon_inh_conns = StateMonitor(inh_exc_conn, variables=['w_plast', 'normalize
                                   name='statemon_inh_conns')
 statemon_inh_cells = StateMonitor(inh_cells, variables=['Vm'], record=True,
                                   name='statemon_inh_cells')
+statemon_exc_cells = StateMonitor(exc_cells, variables=['Vm', 'normalized_activity_proxy'], record=True,
+                                  name='statemon_exc_cells')
+statemon_pop_rate_e = PopulationRateMonitor(exc_cells)
 
 net = TeiliNetwork()
 net.add(poisson_spikes, exc_cells, inh_cells, inh_exc_conn, feedforward_exc,
         spikemon_exc_neurons, spikemon_inh_neurons, exc_inh_conn, 
         spikemon_seq_neurons, statemon_ffe_conns, statemon_inh_conns,
         statemon_ie_conns, statemon_inh_cells, inh_inh_conn,
-        statemon_ee_conns, exc_exc_conn)
+        statemon_ee_conns, exc_exc_conn, statemon_exc_cells,
+        statemon_pop_rate_e, feedforward_inh)
+
 net.run(10000*ms, report='stdout', report_period=100*ms)
+variance_th = .5
+inh_exc_conn.variance_th = np.random.uniform(
+        low=variance_th - 0.1,
+        high=variance_th + 0.1,
+        size=len(inh_exc_conn))
+net.run(30000*ms, report='stdout', report_period=100*ms)
 
 # Plots
 from brian2 import *
 figure()
-y = np.mean(statemon_inh_conns.normalized_activity_proxy, axis=0)
-stdd=np.std(statemon_inh_conns.normalized_activity_proxy, axis=0)
+_ = hist(inh_exc_conn.w_plast, bins=20)
+xlabel('Inh. weight')
+ylabel('Count')
+title('Distribution of inhibitory weights')
+
+figure()
+y = np.mean(statemon_exc_cells.normalized_activity_proxy, axis=0)
+stdd=np.std(statemon_exc_cells.normalized_activity_proxy, axis=0)
 plot(statemon_inh_conns.t/ms, y)
+ylabel('normalized activity mean value')
+xlabel('time (ms)')
 ylim([-0.05, 1.05])
 fill_between(statemon_inh_conns.t/ms, y-stdd, y+stdd, facecolor='lightblue')
+annotate(f"""learning rate: {inh_exc_conn.inh_learning_rate}
+           variance threshold: .9 for first 10s, .1 afterwards""", xy = (0, 0.1))
+
+figure()
+plot(statemon_exc_cells.normalized_activity_proxy.T)
+xlabel('time (ms)')
+ylabel('normalized activity value')
+title('Normalized activity of all neurons')
+
+figure()
+plot(np.array(statemon_pop_rate_e.t/ms), np.array(statemon_pop_rate_e.smooth_rate(width=60*ms)/Hz))
+xlabel('time (ms)')
+ylabel('Mean firing rate (Hz)')
+title('Effect of inhibitory weights on firing pattern of excitatory neurons')
 
 tot_e_curr = np.sum(statemon_ffe_conns.I_syn, axis=0) + np.sum(statemon_ee_conns.I_syn, axis=0)
 tot_i_curr = np.sum(statemon_ie_conns.I_syn, axis=0)
@@ -176,6 +219,7 @@ plot(statemon_inh_conns.t/ms, -tot_i_curr/amp, color='b', label='summed inh. cur
 plot(statemon_inh_conns.t/ms, (tot_e_curr - tot_i_curr)/amp, color='k', label='net. current')
 ylabel('Current [amp]')
 xlabel('time [ms]')
+title('EI balance')
 legend()
 
 figure()
@@ -187,4 +231,63 @@ title('Inh. neurons')
 figure()
 plot(spikemon_seq_neurons.t/ms, spikemon_seq_neurons.i, '.')
 title('Input spikes')
+
+# TODO vectorize by working with flattened array
+#win_slidings = []
+#win_size = 30
+## Get indices to make calculations over a sliding window
+#for y in range(np.shape(statemon_exc_cells.Vm)[1]):
+#    win_slidings.append([])
+#    for x in range(win_size):
+#            win_slidings[-1].append(x+y)
+#
+# Adjust last win_size-1 intervals which are outside array dimensions
+#trim_win = [val[:-(i+1)] for i, val in enumerate(win_slidings[-win_size+1:])]
+#win_slidings[-win_size+1:] = trim_win
+#
+#mean_exc_rate = []
+#mean_inh_rate = []
+#for i in win_slidings:
+#    mean_exc_rate.append(np.mean(statemon_exc_cells.Vm[:,i]))
+#    mean_inh_rate.append(np.mean(statemon_inh_cells.Vm[:,i]))
+#
+#figure()
+#plot(mean_exc_rate)
+#title('Mean of 100 excitatory neurons')
+#xlabel('time(ms)')
+#ylabel('V')
+#
+#figure()
+#plot(mean_inh_rate)
+#title('Mean of 25 inhibitory neurons')
+#xlabel('time(ms)')
+#ylabel('V')
+
 show()
+
+#import pyqtgraph as pg
+#from pyqtgraph.Qt import QtCore, QtGui
+#from pyqtgraph.dockarea import *
+#
+#app = QtGui.QApplication([])
+#win = QtGui.QMainWindow()
+#area = DockArea()
+#win.setCentralWidget(area)
+#
+#d1 = Dock('Normalized activity', size=(1, 1))
+#d2 = Dock('Parameters', size=(1, 1))
+#area.addDock(d1, 'left')
+#area.addDock(d2, 'left')
+#area.moveDock(d2, 'above', d1)
+#
+#w1 = pg.LayoutWidget()
+#label = QtGui.QLabel(f"""learning rate: {inh_exc_conn.inh_learning_rate}
+#                         variance threshold: {variance_th}
+#                         e->e weights: """)
+#w1.addWidget(label)
+#d1.addWidget(w1)
+#win.show()
+#if __name__ == '__main__':
+#   import sys
+#   if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+#       QtGui.QApplication.instance().exec_()

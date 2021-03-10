58d54657b61589364fbf27dbebffe88d6d632bdb
pabloabur <pabloabur@gmail.com>
2021-03-10 18:54:50 +1100

    Updates synapse templates

--- teili/models/builder/templates/synapse_templates.py
+++ teili/models/builder/templates/synapse_templates.py
@@ -29,7 +29,7 @@ your equation will be : %d{synvar_e}/dt = (-{synvar_e})**2 / synvar_e
 
 
 from teili import constants
-from brian2 import pF, nS, mV, ms, pA, nA, volt, second
+from brian2 import pF, nS, mV, ms, pA, nA, mA, volt, second
 
 current = {
     'model': '''
@@ -174,6 +174,44 @@ dpi_shunt_params = {
     'kp_syn': 0.66,
     'I_th': 10 * pA,
     'I_syn': constants.I0
+}
+
+"""LIF neuron model with stochastic decay taken from Wang et al. (2018).
+Please refer to this paper for more information. Note that this model was
+conceptualized in discrete time with backward euler scheme and an integer
+operation. An state updader with x_new = f(x,t) and
+defaultclock.dt = 1*ms in the code using this model.
+"""
+quantized_stochastic_decay = {
+    'model': '''
+        dI_syn/dt = int(I_syn*decay_syn/mA + decay_probability_syn)*mA/second : amp (clock-driven)
+        decay_probability_syn = lfsr_timedarray( ((seed_syn+t) % lfsr_max_value_syn) + lfsr_init_syn ) / (2**lfsr_num_bits_syn): 1
+        Iin{input_number}_post = I_syn * sign(weight)                           : amp (summed)
+
+        decay_syn = tau_syn/(tau_syn + dt) : 1
+
+        weight                : 1
+        w_plast               : 1
+        lfsr_max_value_syn : second
+        seed_syn : second
+        lfsr_init_syn : second
+        gain_syn              : amp
+        tau_syn               : second (constant)
+        lfsr_num_bits_syn : 1 # Number of bits in the LFSR used
+        ''',
+        'on_pre': '''
+        I_syn += gain_syn * abs(weight) * w_plast
+        ''',
+        'on_post': '''
+        '''
+}
+
+quantized_stochastic_decay_params = {
+    'weight' : 1,
+    'w_plast' : 1,
+    'gain_syn' : 1*mA,
+    'tau_syn': 3*ms,
+    'lfsr_num_bits_syn': 6
 }
 
 """ **Plasticity blocks**
@@ -355,6 +393,64 @@ stdp_para_conductance = {
     "w_plast": 0
 }
 
+stochastic_decay_stdp = {
+    'model': '''
+        dApre/dt = int(Apre * decay_stdp_Apre + decay_probability_Apre)/second : 1 (clock-driven)
+        dApost/dt = int(Apost * decay_stdp_Apost + decay_probability_Apost)/second : 1 (clock-driven)
+        decay_probability_Apre = lfsr_timedarray( ((seed_Apre+t) % lfsr_max_value_Apre) + lfsr_init_Apre ) / (2**lfsr_num_bits_Apre): 1
+        decay_probability_Apost = lfsr_timedarray( ((seed_Apost+t) % lfsr_max_value_Apost) + lfsr_init_Apost ) / (2**lfsr_num_bits_Apost): 1
+
+        decay_stdp_Apre = taupre/(taupre + dt) : 1
+        decay_stdp_Apost = taupost/(taupost + dt) : 1
+
+        seed_Apre : second
+        lfsr_max_value_Apre : second
+        lfsr_init_Apre : second
+        lfsr_num_bits_Apre : 1
+        seed_Apost : second
+        lfsr_max_value_Apost : second
+        lfsr_init_Apost : second
+        lfsr_num_bits_Apost : 1
+        w_max: 1 (constant)
+        A_max: 1 (constant)
+        dApre: 1 (constant)
+        A_gain: 1 (constant)
+        taupre : second (constant)
+        taupost : second (constant)
+        ''',
+    'on_pre': '''
+        Apre += dApre
+        Apre = clip(Apre, 0, A_max)
+        w_plast = int(clip(w_plast - Apost/A_gain*int(lastspike_post!=lastspike_pre), 0, w_max))
+        ''',
+    'on_post': '''
+        Apost += dApre
+        Apost = clip(Apost, 0, A_max)
+        w_plast = int(clip(w_plast + Apre/A_gain*int(lastspike_post!=lastspike_pre), 0, w_max))
+        '''
+}
+
+stochastic_decay_stdp_params = {
+    "taupre": 3 * ms,
+    "taupost": 3 * ms,
+    "w_max": 15,
+    "A_max": 15,
+    "A_gain": 4,
+    "dApre": 15,
+    "w_plast": 1,
+    'lfsr_num_bits_Apre': 6,
+    'lfsr_num_bits_Apost': 6
+}
+
+quantized_standard_stdp_params = {
+    "taupre": 10 * ms,
+    "taupost": 10 * ms,
+    "w_max": 15,
+    "dApre": 1,
+    "Q_diffAPrePost": 1.05,
+    "w_plast": 1
+}
+
 """Kernels Blocks:
 You need to declare two set of parameters for every block:
 *   current based models
@@ -485,7 +581,8 @@ modes = {
     'conductance': conductance,
     'DPI': dpi,
     'DPIShunting': dpi_shunt,
-    'unit_less': unit_less
+    'unit_less': unit_less,
+    'QuantizedStochasticDecay': quantized_stochastic_decay
 }
 
 kernels = {
@@ -497,7 +594,8 @@ kernels = {
 plasticity_models = {
     'non_plastic': none_model,
     'fusi': fusi,
-    'stdp': stdp
+    'stdp': stdp,
+    'stochastic_decay_stdp': stochastic_decay_stdp
 }
 
 structural_plasticity = {

a33aa045ed71971df8f978158b8fece6659da743
pabloabur <pabloabur@gmail.com>
2021-09-06 09:23:14 +1000

    Adds different pairing and heterosynaptic plasticity to STDP

--- teili/models/builder/templates/synapse_templates.py
+++ teili/models/builder/templates/synapse_templates.py
@@ -478,23 +478,28 @@ quantized_stochastic_stdp = {
         rand_int_Apre2 : 1
         rand_int_Apost1 : 1
         rand_int_Apost2 : 1
+        rand_int_Apost3 : 1
         rand_num_bits_Apre : 1 # Number of bits of random number generated for Apre
         rand_num_bits_Apost : 1 # Number of bits of random number generated for Apost
         stdp_thres : 1 (constant)
         ''',
     'on_pre': '''
-        Apre += dApre
-        Apre = clip(Apre, 0, A_max)
         rand_int_Apre1 = ceil(rand() * (2**rand_num_bits_Apre-1))
         rand_int_Apre2 = ceil(rand() * (2**rand_num_bits_Apre-1))
-        w_plast = clip(w_plast - 1*int(lastspike_post!=lastspike_pre)*int(rand_int_Apre1 < Apost)*int(rand_int_Apre2 <= stdp_thres), 0, w_max)
+        w_plast = clip(w_plast - 1*int(lastspike_post!=lastspike_pre)*int(rand_int_Apre1 < Apost - Apre)*int(rand_int_Apre2 <= stdp_thres), 0, w_max)
+        Apre += dApre
+        Apre = clip(Apre, 0, A_max)
         ''',
     'on_post': '''
-        Apost += dApre
-        Apost = clip(Apost, 0, A_max)
         rand_int_Apost1 = ceil(rand() * (2**rand_num_bits_Apost-1))
         rand_int_Apost2 = ceil(rand() * (2**rand_num_bits_Apost-1))
-        w_plast = clip(w_plast + 1*int(lastspike_post!=lastspike_pre)*int(rand_int_Apost1 < Apre)*int(rand_int_Apost2 <= stdp_thres), 0, w_max)
+        rand_int_Apost3 = ceil(rand() * (2**rand_num_bits_Apost-1))
+        w_plast = clip(w_plast - 1*int(rand_int_Apost1<=1)*int(rand_int_Apost2<=1)*int(rand_int_Apost3<15-w_plast), 0, w_max)
+        rand_int_Apost1 = ceil(rand() * (2**rand_num_bits_Apost-1))
+        rand_int_Apost2 = ceil(rand() * (2**rand_num_bits_Apost-1))
+        w_plast = clip(w_plast + 1*int(lastspike_post!=lastspike_pre)*int(rand_int_Apost1 < Apre - Apost)*int(rand_int_Apost2 <= stdp_thres), 0, w_max)
+        Apost += dApre
+        Apost = clip(Apost, 0, A_max)
         '''
 }
 
